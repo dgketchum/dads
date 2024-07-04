fefbcf3d73b42b79417a44bca2064dd83170e2aa
dgketchum <dgketchum@gmail.com>
2024-07-04 16:21:22 -0600

    revise ee extract code, all bands

--- extract/ee/landsat_extract.py
+++ extract/ee/landsat_extract.py
@@ -4,18 +4,18 @@ import sys
 import ee
 import geopandas as gpd
 
-from data_extraction.ee.ee_utils import is_authorized
+from extract.ee.ee_utils import landsat_masked, is_authorized
 
 sys.path.insert(0, os.path.abspath('../..'))
 sys.setrecursionlimit(5000)
 
 IRR = 'projects/ee-dgketchum/assets/IrrMapper/IrrMapperComp'
 
-ETF = 'projects/usgs-gee-nhm-ssebop/assets/ssebop/landsat/c02'
-
 EC_POINTS = 'users/dgketchum/flux_ET_dataset/stations'
 
 STATES = ['AZ', 'CA', 'CO', 'ID', 'MT', 'NM', 'NV', 'OR', 'UT', 'WA', 'WY']
+
+SELECT = ['B{}'.format(b) for b in [2, 3, 4, 5, 6, 7, 10]]
 
 
 def get_flynn():
@@ -27,52 +27,7 @@ def get_flynn():
                                            {'key': 'Flynn_Ex'}))
 
 
-def export_etf_images(feature_coll, year=2015, bucket=None, debug=False, mask_type='irr'):
-    s, e = '1987-01-01', '2021-12-31'
-    irr_coll = ee.ImageCollection(IRR)
-    coll = irr_coll.filterDate(s, e).select('classification')
-    remap = coll.map(lambda img: img.lt(1))
-    irr_min_yr_mask = remap.sum().gte(5)
-    irr = irr_coll.filterDate('{}-01-01'.format(year),
-                              '{}-12-31'.format(year)).select('classification').mosaic()
-    irr_mask = irr_min_yr_mask.updateMask(irr.lt(1))
-
-    coll = ee.ImageCollection(ETF).filterDate('{}-01-01'.format(year), '{}-12-31'.format(year))
-    coll = coll.filterBounds(feature_coll)
-    scenes = coll.aggregate_histogram('system:index').getInfo()
-
-    for img_id in scenes:
-
-        splt = img_id.split('_')
-        _name = '_'.join(splt[-3:])
-
-        img = ee.Image(os.path.join(ETF, img_id))
-
-        if mask_type == 'no_mask':
-            img = img.clip(feature_coll.geometry()).int()
-        elif mask_type == 'irr':
-            img = img.clip(feature_coll.geometry()).mask(irr_mask).int()
-        elif mask_type == 'inv_irr':
-            img = img.clip(feature_coll.geometry()).mask(irr.gt(0)).int()
-
-        if debug:
-            point = ee.Geometry.Point([-106.576, 46.26])
-            data = img.sample(point, 30).getInfo()
-            print(data['features'])
-
-        task = ee.batch.Export.image.toCloudStorage(
-            img,
-            description='ETF_{}_{}'.format(mask_type, _name),
-            bucket=bucket,
-            region=feature_coll.geometry(),
-            crs='EPSG:5070',
-            scale=30)
-
-        task.start()
-        print(_name)
-
-
-def flux_tower_etf(shapefile, bucket=None, debug=False, mask_type='irr', check_dir=None):
+def multipoint_landsat(shapefile, bucket=None, debug=False, check_dir=None):
     df = gpd.read_file(shapefile)
 
     assert df.crs.srs == 'EPSG:5071'
@@ -87,65 +42,45 @@ def flux_tower_etf(shapefile, bucket=None, debug=False, mask_type='irr', check_d
 
     for fid, row in df.iterrows():
 
-        for year in range(2000, 2015):
+        for year in range(1990, 2025):
 
-            state = row['field_3']
-            if state not in STATES:
-                continue
+            site = row['FID']
 
-            site = row['field_1']
-
-            if site not in ['US-Mj1', 'US-Mj2']:
-                continue
-
-            desc = 'etf_{}_{}_{}'.format(site, year, mask_type)
+            desc = 'bands_{}_{}'.format(site, year)
             if check_dir:
                 f = os.path.join(check_dir, '{}.csv'.format(desc))
                 if os.path.exists(f):
                     print(desc, 'exists, skipping')
                     continue
 
-            irr = irr_coll.filterDate('{}-01-01'.format(year),
-                                      '{}-12-31'.format(year)).select('classification').mosaic()
-            irr_mask = irr_min_yr_mask.updateMask(irr.lt(1))
+            point = ee.Geometry.Point([row['lon'], row['lat']])
+            geo = point.buffer(500.)
+            fc = ee.FeatureCollection(ee.Feature(geo, {'FID': site}))
 
-            point = ee.Geometry.Point([row['field_8'], row['field_7']])
-            geo = point.buffer(150.)
-            fc = ee.FeatureCollection(ee.Feature(geo, {'field_1': site}))
-
-            etf_coll = ee.ImageCollection(ETF).filterDate('{}-01-01'.format(year),
-                                                          '{}-12-31'.format(year))
-            etf_coll = etf_coll.filterBounds(geo)
-            etf_scenes = etf_coll.aggregate_histogram('system:index').getInfo()
+            coll = landsat_masked(year, fc).select(SELECT)
+            scenes = coll.aggregate_histogram('system:index').getInfo()
 
             first, bands = True, None
             selectors = [site]
-
-            for img_id in etf_scenes:
+            for img_id in scenes:
 
                 splt = img_id.split('_')
-                _name = '_'.join(splt[-3:])
+                _names = ['{}_{}'.format('_'.join(splt[-3:]), b) for b in SELECT]
 
-                selectors.append(_name)
+                selectors.append(_names)
 
-                etf_img = ee.Image(os.path.join(ETF, img_id)).rename(_name)
-                etf_img = etf_img.divide(10000)
+                nd_img = coll.filterMetadata('system:index', 'equals', img_id).first().rename(_names)
 
-                if mask_type == 'no_mask':
-                    etf_img = etf_img.clip(fc.geometry())
-                elif mask_type == 'irr':
-                    etf_img = etf_img.clip(fc.geometry()).mask(irr_mask)
-                elif mask_type == 'inv_irr':
-                    etf_img = etf_img.clip(fc.geometry()).mask(irr.gt(0))
+                nd_img = nd_img.clip(fc.geometry())
 
                 if first:
-                    bands = etf_img
+                    bands = nd_img
                     first = False
                 else:
-                    bands = bands.addBands([etf_img])
+                    bands = bands.addBands([nd_img])
 
                 if debug:
-                    data = etf_img.sample(fc, 30).getInfo()
+                    data = nd_img.sample(point, 30).getInfo()
                     print(data['features'])
 
             data = bands.reduceRegions(collection=fc,
@@ -162,98 +97,20 @@ def flux_tower_etf(shapefile, bucket=None, debug=False, mask_type='irr', check_d
 
             task.start()
             print(desc)
-
-
-def clustered_field_etf(feature_coll, bucket=None, debug=False, mask_type='irr', check_dir=None):
-
-    feature_coll = ee.FeatureCollection(feature_coll)
-
-    s, e = '1987-01-01', '2021-12-31'
-    irr_coll = ee.ImageCollection(IRR)
-    coll = irr_coll.filterDate(s, e).select('classification')
-    remap = coll.map(lambda img: img.lt(1))
-    irr_min_yr_mask = remap.sum().gte(5)
-
-    for year in range(1987, 2022):
-
-        irr = irr_coll.filterDate('{}-01-01'.format(year),
-                                  '{}-12-31'.format(year)).select('classification').mosaic()
-        irr_mask = irr_min_yr_mask.updateMask(irr.lt(1))
-
-        desc = 'etf_{}_{}'.format(year, mask_type)
-
-        if check_dir:
-            f = os.path.join(check_dir, '{}.csv'.format(desc))
-            if os.path.exists(f):
-                print(desc, 'exists, skipping')
-                continue
-
-        coll = ee.ImageCollection(ETF).filterDate('{}-01-01'.format(year), '{}-12-31'.format(year))
-        coll = coll.filterBounds(feature_coll)
-        scenes = coll.aggregate_histogram('system:index').getInfo()
-
-        first, bands = True, None
-        selectors = ['FID']
-
-        for img_id in scenes:
-
-            # if img_id != 'lt05_036029_20000623':
-            #     continue
-
-            splt = img_id.split('_')
-            _name = '_'.join(splt[-3:])
-
-            selectors.append(_name)
-
-            etf_img = ee.Image(os.path.join(ETF, img_id)).rename(_name)
-            etf_img = etf_img.divide(10000)
-
-            if mask_type == 'no_mask':
-                etf_img = etf_img.clip(feature_coll.geometry())
-            elif mask_type == 'irr':
-                etf_img = etf_img.clip(feature_coll.geometry()).mask(irr_mask)
-            elif mask_type == 'inv_irr':
-                etf_img = etf_img.clip(feature_coll.geometry()).mask(irr.gt(0))
-
-            if first:
-                bands = etf_img
-                first = False
-            else:
-                bands = bands.addBands([etf_img])
-
-            if debug:
-                point = ee.Geometry.Point([-107.188225, 44.9011])
-                data = etf_img.sample(point, 30).getInfo()
-                print(data['features'])
-
-        # TODO extract pixel count to filter data
-        data = bands.reduceRegions(collection=feature_coll,
-                                   reducer=ee.Reducer.mean(),
-                                   scale=30)
-
-        task = ee.batch.Export.table.toCloudStorage(
-            data,
-            description=desc,
-            bucket=bucket,
-            fileNamePrefix=desc,
-            fileFormat='CSV',
-            selectors=selectors)
-
-        task.start()
-        print(desc)
+            exit()
 
 
 if __name__ == '__main__':
 
-    d = '/media/research/IrrigationGIS/swim'
+    d = '/media/research/IrrigationGIS'
     if not os.path.exists(d):
-        d = '/home/dgketchum/data/IrrigationGIS/swim'
+        d = '/home/dgketchum/data/IrrigationGIS'
 
     is_authorized()
     bucket_ = 'wudr'
-    fields = 'users/dgketchum/fields/tongue_annex_20OCT2023'
-    for mask in ['inv_irr', 'irr']:
-        chk = os.path.join(d, 'examples/tongue/landsat/extracts/etf/{}'.format(mask))
-        clustered_field_etf(fields, bucket_, debug=False, mask_type=mask, check_dir=None)
+    fields = os.path.join(d, 'climate', 'agrimet', 'agrimet_mt_aea.shp')
+
+    chk = os.path.join(d, 'dads', 'landsat', 'agrimet_locations', 'ee_extracts')
+    multipoint_landsat(fields, bucket_, debug=False, check_dir=None)
 
 # ========================= EOF ====================================================================
