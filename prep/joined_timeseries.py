475f3fb73a53a6368b6a3757e2ad71e871a36ec0
dgketchum <dgketchum@gmail.com>
2024-07-17 20:53:24 -0600

    join met, obs, rs, test relationships

--- prep/joined_timeseries.py
+++ prep/joined_timeseries.py
@@ -1,147 +1,171 @@
+import datetime
 import os
-import json
-
-import numpy as np
-import pandas as pd
-import geopandas as gpd
-
-# from data_extraction.gridmet.gridmet import find_gridmet_points, download_gridmet
-# from data_extraction.snodas.snodas import snodas_zonal_stats
-
 import warnings
 
+import matplotlib.pyplot as plt
+import pandas as pd
+from refet import Daily, calcs
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+
 warnings.filterwarnings("ignore", category=FutureWarning)
+VAR_MAP = {'rsds': 'Rs (w/m2)',
+           'ea': 'Vapor Pres (kPa)',
+           'min_temp': 'TMin (C)',
+           'max_temp': 'TMax (C)',
+           'mean_temp': 'TAvg (C)',
+           'wind': 'ws_2m (m/s)',
+           'eto': 'ETo (mm)'}
+
+RENAME_MAP = {v: k for k, v in VAR_MAP.items()}
+
+COMPARISON_VARS = ['vpd', 'rsds', 'min_temp', 'max_temp', 'mean_temp', 'wind', 'eto']
 
 
-def join_daily_timeseries(fields, gridmet_dir, landsat_table, snow, dst_dir, overwrite=False,
-                          start_date=None, end_date=None, **kwargs):
-    with open(snow, 'r') as f:
-        snow = json.load(f)
-
-    lst = pd.read_csv(landsat_table, parse_dates=True, index_col=0)
-    start, end = lst.index[0], lst.index[-1]
-
-    if 'params' not in kwargs.keys():
-        params = set(['_'.join(x.split('_')[1:]) for x in lst.columns])
-    else:
-        params = kwargs['params']
-
-    fields = gpd.read_file(fields)
-    fields.index = fields['FID']
-
-    out_plots = []
-
+def join_daily_timeseries(fields, gridmet_dir, rs_dir, sta_dir, dst_dir, overwrite=False, index='FID', **kwargs):
+    fields = pd.read_csv(fields, index_col=index)
+    df = pd.DataFrame()
     for f, row in fields.iterrows():
 
-        if 'target_fields' in kwargs.keys():
-            if str(f) not in kwargs['target_fields']:
-                continue
+        # if 'MT' not in f:
+        #     continue
 
-        if pd.isna(row['GFID']):
-            print(row['FID'], 'was not assigned a Gridmet point')
-            continue
+        lst = join_landsat(rs_dir, f)
+        lst_cols = lst.columns
 
-        _file = os.path.join(dst_dir, '{}_daily.csv'.format(f))
-        if os.path.exists(_file) and not overwrite:
-            continue
+        gridmet_file = os.path.join(gridmet_dir, '{}.csv'.format(f))
 
-        gridmet_file = os.path.join(gridmet_dir, 'gridmet_historical_{}.csv'.format(int(row['GFID'])))
+        # TODO: remove the tz application in the data extract code
+        gdf = pd.read_csv(gridmet_file, index_col=0, parse_dates=True)
+        gdf.index = pd.DatetimeIndex([datetime.date(i.year, i.month, i.day) for i in gdf.index])
+        match_idx = [i for i in lst.index if i in gdf.index]
+        gdf = gdf.loc[match_idx]
+        gdf['rsds'] *= 0.0864
+        gdf['mean_temp'] = (gdf['min_temp'] + gdf['max_temp']) * 0.5
+        gdf['vpd'] = gdf.apply(_vpd, axis=1)
+        gdf['rn'] = gdf.apply(_rn, lat=row['STATION_LAT'], elev=row['STATION_ELEV_M'],
+                              zw=row['Anemom_height_m'], axis=1)
+        gdf = gdf[COMPARISON_VARS]
+        grd_cols = ['{}_gm'.format(c) for c in gdf.columns]
+        gdf.columns = grd_cols
+        gdf.loc[match_idx, lst.columns] = lst.loc[match_idx]
+
+        sta_file = os.path.join(sta_dir, '{}.csv'.format(f))
 
         try:
-            gridmet = pd.read_csv(gridmet_file, index_col='date', parse_dates=True).loc[start: end]
-            drop_cols = [c for c in gridmet.columns if '.' in c]
-            if drop_cols:
-                gridmet.drop(columns=drop_cols, inplace=True)
+            sdf = pd.read_csv(sta_file, index_col='Unnamed: 0', parse_dates=True)
+        except ValueError:
+            sdf = pd.read_csv(sta_file, index_col='date', parse_dates=True)
 
-            swe_data = [(pd.to_datetime(k), v[str(f)]) for k, v in snow.items()]
-            swe = pd.Series(index=[x[0] for x in swe_data], data=[x[1] for x in swe_data])
-            print('Max SWE {}: {}'.format(f, swe.max()))
-            match_idx = [i for i in gridmet.index if i in swe.index]
-            gridmet.loc[match_idx, 'obs_swe'] = swe
+        match_idx = [i for i in match_idx if i in sdf.index]
+        sdf = sdf.loc[match_idx]
+        sdf.rename(columns=RENAME_MAP, inplace=True)
+        sdf['doy'] = [i.dayofyear for i in sdf.index]
+        sdf['rsds'] *= 0.0864
+        sdf['vpd'] = sdf.apply(_vpd, axis=1)
+        sdf['rn'] = sdf.apply(_rn, lat=row['STATION_LAT'], elev=row['STATION_ELEV_M'],
+                              zw=row['Anemom_height_m'], axis=1)
 
-        except FileNotFoundError:
-            print(gridmet_file, 'not found\n')
-            continue
+        sdf = sdf[COMPARISON_VARS]
+        obs_cols = ['{}_obs'.format(c) for c in sdf.columns]
+        sdf.columns = obs_cols
+        all_cols = ['FID'] + obs_cols + grd_cols + list(lst_cols)
+        sdf = pd.concat([sdf, gdf], ignore_index=False, axis=1)
+        sdf['FID'] = f
+        sdf = sdf[all_cols]
+        df = pd.concat([df, sdf])
+        print(f)
 
-        except pd.errors.EmptyDataError:
-            print(gridmet_file, 'empty\n')
-            continue
+    variables = ['rsds', 'vpd', 'min_temp', 'max_temp', 'mean_temp', 'wind', 'eto']
+    bands = ['B10', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7']
+    results = {}
 
-        for p in params:
-            gridmet.loc[lst.index, p] = lst['{}_{}'.format(f, p)]
+    for var in variables:
+        for month in range(1, 13):
+            idx = [i for i in df.index if i.month == month]
+            sub = df.loc[idx, [f"{var}_obs", f"{var}_gm"] + bands].copy()
+            sub.dropna(how='any', inplace=True, axis=0)
+            sub['residual'] = sub[f"{var}_obs"] - sub[f"{var}_gm"]
+            sub[bands] = sub[bands].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
 
-        if start_date:
-            gridmet = gridmet.loc[start_date:]
-        if end_date:
-            gridmet = gridmet.loc[:end_date]
+            if sub.shape[0] == 0:
+                continue
 
-        accept = True
+            for band in bands:
 
-        for i, r in gridmet.iterrows():
-            if np.isnan(r['ndvi_irr']) and np.isnan(r['ndvi_inv_irr']):
-                print('{} in {} has only nan in ndvi_irr and ndvi_inv_irr'.format(f, i.year))
-                accept = False
-                break
-            if np.isnan(r['etf_irr']) and np.isnan(r['etf_inv_irr']):
-                print('{} in {} has only nan in etf_irr and etf_inv_irr'.format(f, i.year))
-                accept = False
-                break
+                r_squared = r2_score(sub['residual'], sub[band])
 
-        if accept:
-            gridmet.to_csv(_file)
-            print(_file)
+                if r_squared > 0.:
+                    print('r2: {:.2f}, {}, {}, month {}, n {}'.format(r_squared, var, band, month, sub.shape[0]))
+                    plt.scatter(sub[band], sub['residual'])
+                    plt.xlabel(band)
+                    plt.ylabel(var)
+                    plt.savefig(os.path.join(dst_dir, '{}_{}_{}.png'.format(month, var, band)))
+                    plt.close()
 
-            out_plots.append(f)
+                results[f"{var}_residual_vs_{band}"] = r_squared
 
-    print(out_plots)
+    cf = pd.DataFrame.from_dict(results, orient='index', columns=['correlation'])
+
+
+def join_landsat(dir_, glob):
+    l = [os.path.join(dir_, x) for x in os.listdir(dir_) if glob in x]
+    df = pd.concat([pd.read_csv(f).T for f in l])
+    df.dropna(how='any', axis=0, inplace=True)
+    splt = [i.split('_') for i in df.index]
+    df['band'] = [i[-1] for i in splt]
+    df.index = [pd.to_datetime(i[-2]) for i in splt]
+    try:
+        df = df.pivot(columns=['band'])
+    except ValueError:
+        df['index'] = [i.strftime('%Y%m%d') for i in df.index]
+        df = df.groupby(['index', 'band']).mean().reset_index()
+        df.index = pd.DatetimeIndex(df['index'])
+        df.drop(columns=['index'], inplace=True)
+        df = df.pivot(columns=['band'])
+
+    df.columns = [c[1] for c in df.columns]
+    q1 = df.quantile(0.25)
+    q3 = df.quantile(0.75)
+    iqr = q3 - q1
+    lower_bound = q1 - 1.5 * iqr
+    upper_bound = q3 + 1.5 * iqr
+    df = df[~((df < lower_bound) | (df > upper_bound)).any(axis=1)]
+    return df
+
+
+def _vpd(r):
+    es = calcs._sat_vapor_pressure(r['mean_temp'])
+    vpd = es - r['ea']
+    return vpd[0]
+
+
+def _rn(r, lat, elev, zw):
+    asce = Daily(tmin=r['min_temp'],
+                 tmax=r['max_temp'],
+                 rs=r['rsds'],
+                 ea=r['ea'],
+                 uz=r['wind'],
+                 zw=zw,
+                 doy=r['doy'],
+                 elev=elev,
+                 lat=lat)
+
+    rn = asce.rn[0]
+    return rn
 
 
 if __name__ == '__main__':
 
-    d = '/media/research/IrrigationGIS/swim'
+    d = '/media/research/IrrigationGIS/dads'
     if not os.path.exists(d):
-        d = d = '/home/dgketchum/data/IrrigationGIS/swim'
+        d = '/home/dgketchum/data/IrrigationGIS/dads'
 
-    project = 'tongue'
-    project_ws = os.path.join(d, 'examples', project)
+    fields = os.path.join(d, 'met', 'stations', 'gwx_stations.csv')
+    sta = os.path.join(d, 'met', 'obs', 'gwx')
+    gm = os.path.join(d, 'met', 'gridded', 'gridmet')
+    rs = os.path.join(d, 'rs', 'gwx_stations')
+    joined = os.path.join(d, 'tables', 'gridmet')
 
-    gridmet = os.path.join(d, 'gridmet')
-    rasters_ = os.path.join(gridmet, 'gridmet_corrected', 'correction_surfaces_aea')
-    # grimet_cent = os.path.join(gridmet, 'gridmet_centroids.shp')
-    grimet_cent = os.path.join(gridmet, 'gridmet_centroids_tongue.shp')
-
-    fields_shp = os.path.join(project_ws, 'gis', '{}_fields.shp'.format(project))
-    fields_gridmet = os.path.join(project_ws, 'gis', '{}_fields_gfid.shp'.format(project))
-    gridmet_factors = os.path.join(project_ws, 'gis', '{}_fields_gfid.json'.format(project))
-    met = os.path.join(project_ws, 'met_timeseries')
-
-    targets = TONGUE_SELECT
-
-    # find_gridmet_points(fields_shp, grimet_cent, rasters_, fields_gridmet, gridmet_factors, field_select=None)
-    #
-    # download_gridmet(fields_gridmet, gridmet_factors, met, start='1987-01-01', end='2023-12-31',
-    #                  target_fields=targets, overwite=True)
-
-    fields_shp_wgs = os.path.join(project_ws, 'gis', '{}_fields_wgs.shp'.format(project))
-    snow_ts = os.path.join(project_ws, 'snow_timeseries', 'snodas_{}.json'.format(project))
-
-    # this extract is meant to be run on Montana Climate Office machine (Zoran)
-    s_dir = '/data/ssd1/snodas/processed/swe'
-    if not os.path.isdir(s_dir):
-        s_dir = '/media/research/IrrigationGIS/climate/snodas/processed/swe'
-    # snodas_zonal_stats(fields_shp_wgs, s_dir, snow_ts, targets=None, index_col='FID')
-
-    landsat = os.path.join(project_ws, 'landsat', '{}_sensing.csv'.format(project))
-    dst_dir_ = os.path.join(project_ws, 'input_timeseries')
-
-    params = ['etf_inv_irr',
-              'ndvi_inv_irr',
-              'etf_irr',
-              'ndvi_irr']
-
-    params += ['{}_ct'.format(p) for p in params]
-
-    # join_daily_timeseries(fields_gridmet, met, landsat, snow_ts, dst_dir_, overwrite=True,
-    #                       start_date='1989-01-01', end_date='2021-12-31', **{'params': params})
-
+    join_daily_timeseries(fields, gm, rs, sta, joined, index='STATION_ID')
 # ========================= EOF ====================================================================
