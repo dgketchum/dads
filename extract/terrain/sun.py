2a0785233fb9ca95619af23d285ee08e39062233
dgketchum <dgketchum@gmail.com>
2024-07-30 19:25:45 -0600

    runs fast on 250 m

--- extract/terrain/sun.py
+++ extract/terrain/sun.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+
 def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=False):
     s = os.path.join(terrain_dir, 'slope')
     a = os.path.join(terrain_dir, 'aspect')
@@ -14,8 +15,8 @@ def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=Fals
     for dem_name, dem_file in zip(dem_names, dem_files):
         tile = dem_name.split('_')[-1]
 
-        if tile != '12TTS':
-            continue
+        # if tile != '12TTS':
+        #     continue
 
         tile_dir = os.path.join(r, tile)
         if not os.path.isdir(tile_dir):
@@ -38,20 +39,19 @@ def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=Fals
             subprocess.call(['r.in.gdal', f'input={aspect_output_path}', f'output={aspect_name}'])
 
         subprocess.call(['g.region', f'rast=dem_{tile}@{mapset}'])
+
         for day in range(1, 366):
             irradiance_output_path = 'irradiance_day_{0}_{1}'.format(day, tile)
-            if tile == '12TTS' and day in [1, 173, 304, 356]:
-                subprocess.call(
-                    ['r.sun',
-                     'elevation={0}@{1}'.format(dem_name, mapset),
-                     'slope={0}@{1}'.format(slope_name, mapset),
-                     'aspect={0}@{1}'.format(aspect_name, mapset),
-                     'day={0}'.format(day),
-                     'glob_rad={0}@{1}'.format(irradiance_output_path, mapset),
-                     '--overwrite',
-                     ]
-                )
 
+            subprocess.call(['r.sun',
+                             'elevation={0}@{1}'.format(dem_name, mapset),
+                             'slope={0}@{1}'.format(slope_name, mapset),
+                             'aspect={0}@{1}'.format(aspect_name, mapset),
+                             'day={0}'.format(day),
+                             'glob_rad={0}@{1}'.format(irradiance_output_path, mapset),
+                             '--overwrite'])
+
+            if tile == '12TTS':
                 irradiance_output_tif = os.path.join(r, tile, 'irradiance_day_{0}_{1}.tif'.format(day, tile))
                 subprocess.call(
                     ['r.out.gdal', '-c',
@@ -62,7 +62,28 @@ def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=Fals
                      'output={0}'.format(irradiance_output_tif)])
                 print(day)
 
-        break
+        print(tile)
+
+
+def remove_rasters(terrain_dir, resolution=300):
+    d = os.path.join(terrain_dir, 'proj_{}'.format(resolution))
+
+    dem_files = sorted(os.listdir(d))
+    dem_names = sorted([f.split('.')[0] for f in dem_files if f.endswith('.tif')])
+    dem_files = [os.path.join(d, f) for f in dem_files]
+
+    for dem_name, dem_file in zip(dem_names, dem_files):
+        tile = dem_name.split('_')[-1]
+        subprocess.call(['g.remove', 'type=raster', f'name=dem_{tile}', '-f'])
+        subprocess.call(['g.remove', 'type=raster', f'name=slope_{tile}', '-f'])
+        subprocess.call(['g.remove', 'type=raster', f'name=aspect_{tile}', '-f'])
+        subprocess.call(['g.remove', 'type=raster', f'name=aspect_{tile}', '-f'])
+
+        for day in range(1, 366):
+            irradiance_output_path = 'irradiance_day_{0}_{1}'.format(day, tile)
+            subprocess.call(['g.remove', 'type=raster', f'name={irradiance_output_path}', '-f'])
+
+        print('removed {}'.format(tile))
 
 
 if __name__ == '__main__':
@@ -72,5 +93,7 @@ if __name__ == '__main__':
 
     dem_d = os.path.join(root, 'dem')
     calculate_terrain_irradiance(dem_d, mapset="dads_map", overwrite=True)
+
+    # remove_rasters(dem_d, resolution=30)
 
 # ========================= EOF ====================================================================
