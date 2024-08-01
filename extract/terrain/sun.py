a107dc3f252ebb884f91d14a8f186bbd56bca753
dgketchum <dgketchum@gmail.com>
2024-08-01 11:09:22 -0600

    write .tif from GRASS proc data

--- extract/terrain/sun.py
+++ extract/terrain/sun.py
@@ -3,11 +3,12 @@ import time
 import subprocess
 from pprint import pprint
 
+import pandas as pd
+
 
 def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=False):
     slp = os.path.join(terrain_dir, 'slope')
     asp = os.path.join(terrain_dir, 'aspect')
-    rsn = os.path.join(terrain_dir, 'rsun')
     d = os.path.join(terrain_dir, 'proj')
 
     dem_files = sorted(os.listdir(d))
@@ -30,10 +31,6 @@ def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=Fals
         if len(raster_list) >= 365 and not overwrite:
             print(tile, 'is processed, skipping')
             continue
-
-        tile_dir = os.path.join(rsn, tile)
-        if not os.path.isdir(tile_dir):
-            os.mkdir(tile_dir)
 
         slope_output_path = os.path.join(slp, 'slope_{0}.tif'.format(tile))
         subprocess.call(['gdaldem', 'slope', f'{dem_file}', slope_output_path])
@@ -60,19 +57,54 @@ def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=Fals
                              '--overwrite',
                              'nprocs=6'])
 
-            if tile == '12TTS':
-                irradiance_output_tif = os.path.join(rsn, tile, 'irradiance_day_{0}_{1}.tif'.format(day, tile))
-                subprocess.call(
-                    ['r.out.gdal', '-c',
-                     'input={0}@{1}'.format(irradiance_output_path, mapset),
-                     'format=GTiff',
-                     'createopt=COMPRESS=LZW',
-                     '--overwrite',
-                     'output={0}'.format(irradiance_output_tif)])
-                print(day)
             dif = time.time() - start
             print('took {:.2f} seconds'.format(dif))
         print(tile, '\n')
+
+
+def export_rasters(terrain_dir, out_dir, mapset='PERMANENT', overwrite=False, mgrs_list=None):
+
+    if mgrs_list:
+        mgrs_list = pd.read_csv(mgrs_list)['MGRS_TILE'].tolist()
+
+    d = os.path.join(terrain_dir, 'proj')
+
+    dem_files = sorted(os.listdir(d))
+    dem_names = sorted([f.split('.')[0] for f in dem_files if f.endswith('.tif')])
+    dem_files = [os.path.join(d, f) for f in dem_files]
+
+    rsun_out = os.path.join(out_dir, 'rsun')
+
+    for dem_name, dem_file in zip(dem_names, dem_files):
+
+        tile = dem_name.split('_')[-1]
+        if mgrs_list and tile not in mgrs_list:
+            continue
+
+        print('\n', tile)
+
+        tile_dir = os.path.join(rsun_out, tile)
+        if not os.path.isdir(tile_dir):
+            os.mkdir(tile_dir)
+
+        for day in range(1, 366):
+
+            irradiance_output_tif = os.path.join(tile_dir, 'irradiance_day_{0}_{1}.tif'.format(day, tile))
+            irradiance_input = 'irradiance_day_{0}_{1}'.format(day, tile)
+
+            if os.path.exists(irradiance_output_tif) and not overwrite:
+                print(irradiance_output_tif, 'exists')
+                continue
+
+            subprocess.call(
+                ['r.out.gdal', '-c',
+                 'input={0}@{1}'.format(irradiance_input, mapset),
+                 'format=GTiff',
+                 'createopt=COMPRESS=LZW',
+                 '--overwrite',
+                 'output={0}'.format(irradiance_output_tif)])
+
+            print(tile, day)
 
 
 def remove_rasters(terrain_dir, resolution=300):
@@ -98,11 +130,17 @@ def remove_rasters(terrain_dir, resolution=300):
 
 if __name__ == '__main__':
     root = '/media/nvm/IrrigationGIS/dads'
+    out = '/media/research/IrrigationGIS/dads'
     if not os.path.exists(root):
         root = '/home/dgketchum/data/IrrigationGIS/dads'
+        out = '/home/dgketchum/data/IrrigationGIS/dads'
 
     dem_d = os.path.join(root, 'dem')
-    calculate_terrain_irradiance(dem_d, mapset="dads_map", overwrite=False)
+    out_dem = os.path.join(out, 'dem')
+    mgrs = os.path.join(out, 'training', 'wmt_tiles.csv')
+
+    # calculate_terrain_irradiance(dem_d, mapset="dads_map", overwrite=False)
+    export_rasters(dem_d, out_dem, mapset="dads_map", overwrite=False, mgrs_list=mgrs)
 
     # remove_rasters(dem_d, resolution=30)
 
