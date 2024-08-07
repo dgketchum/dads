7916bd35bff661bae0166402f85c007eedcd3911
dgketchum <dgketchum@gmail.com>
2024-08-07 17:45:31 -0600

    work on lstm

--- model/nn.py
+++ model/nn.py
@@ -23,49 +23,65 @@ torch.cuda.get_device_name(torch.cuda.current_device())
 
 
 class PTHLSTMDataset(Dataset):
-    def __init__(self, file_list, window_size=5, transform=None):
+    def __init__(self, file_list, chunks_per_file, transform=None):
         self.file_list = file_list
-        self.window_size = window_size
         self.transform = transform
-        self.chunks = self._find_chunks()
-
-    def _find_chunks(self):
-        chunks = []
-        for file_path in self.file_list:
-            data = torch.load(file_path)
-            start_idx = 0
-            while start_idx + self.window_size <= len(data):
-                if not torch.isnan(data[start_idx:start_idx + self.window_size]).any():
-                    chunks.append((file_path, start_idx))
-                start_idx += 1
-        return chunks
+        self.chunks_per_file = chunks_per_file
 
     def __len__(self):
-        return len(self.chunks)
+        return len(self.file_list)
 
     def __getitem__(self, idx):
-        file_path, start_idx = self.chunks[idx]
-        data = torch.load(file_path)
-        x = data[start_idx:start_idx + self.window_size, 2:]
-        y_obs = data[start_idx + self.window_size - 1, 0]
-        y_gm = data[start_idx + self.window_size - 1, 1]
-        return x, y_obs, y_gm
+        file_idx = idx // self.chunks_per_file
+        chunk_idx = idx % self.chunks_per_file
 
+        file_path = self.file_list[file_idx]
+        data = torch.load(file_path, weights_only=True)
+        chunk = data[chunk_idx]
+        return chunk[:, 2:], chunk[:, 0], chunk[:, 1]
+
+def stack_batch(batch):
+    x = torch.stack([item[0] for item in batch])
+    y = torch.stack([item[1] for item in batch])
+    g = torch.stack([item[2] for item in batch])
+    return x, y, g
 
 class LSTMPredictor(pl.LightningModule):
-    def __init__(self, num_bands=10, hidden_size=64, num_layers=2, learning_rate=0.001):
+    def __init__(self, num_bands=10, hidden_size=64, num_layers=2, learning_rate=0.001, expansion_factor=2, dropout_rate=0.5):
         super().__init__()
-        self.lstm = nn.LSTM(num_bands, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, 1)
+
+        self.input_expansion = nn.Sequential(
+            nn.Linear(num_bands, num_bands * expansion_factor),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate)
+        )
+
+        self.bilstm = nn.LSTM(num_bands * expansion_factor, hidden_size, num_layers, batch_first=True, bidirectional=True)
+
+        self.output_layers = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size * 2, hidden_size * 2),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size * 2, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout_rate),
+            nn.Linear(hidden_size, 1)
+        )
+
         self.criterion = nn.MSELoss()
         self.learning_rate = learning_rate
 
     def forward(self, x):
-        output, _ = self.lstm(x)
-        return self.fc(output[:, -1, :]).squeeze()
+        x = self.input_expansion(x)
+        out, _ = self.bilstm(x)
+        out = out[:, -1, :]
+        out = self.output_layers(out)
+        return out
 
     def training_step(self, batch, batch_idx):
-        x, y, _ = batch
+        x, y, _ = stack_batch(batch)
+        y = y[:, -1]
         y_hat = self(x).squeeze()
         loss = self.criterion(y_hat, y)
         self.log("train_loss", loss)
@@ -73,8 +89,8 @@ class LSTMPredictor(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         x, y_obs, y_gm = batch
-
         y_hat_obs = self(x).squeeze()
+        y_obs = y_obs[:, -1].squeeze()
 
         loss_obs = self.criterion(y_hat_obs, y_obs)
         self.log("val_loss_obs", loss_obs, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
@@ -85,13 +101,17 @@ class LSTMPredictor(pl.LightningModule):
 
         r2_obs = r2_score(y_obs_np, y_hat_obs_np)
         rmse_obs = root_mean_squared_error(y_obs_np, y_hat_obs_np)
-        r2_gm = r2_score(y_obs_np, y_gm_np)
-        rmse_gm = root_mean_squared_error(y_obs_np, y_gm_np)
+        r2_gm = r2_score(y_obs_np, y_gm_np[:, -1])
+        rmse_gm = root_mean_squared_error(y_obs_np, y_gm_np[:, -1])
 
-        self.log("val_r2_obs", r2_obs, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("val_r2_gm", r2_gm, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("val_rmse_obs", rmse_obs, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("val_rmse_gm", rmse_gm, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val_r2_obs", r2_obs, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.log("val_r2_gm", r2_gm, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+
+        self.log("val_rmse_obs", rmse_obs, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.log("val_rmse_gm", rmse_gm, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+
+        self.log("mean_pred", y_hat_obs_np.mean(), on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.log("mean_gm", y_gm_np.mean(), on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
         return loss_obs
 
@@ -107,33 +127,31 @@ class LSTMPredictor(pl.LightningModule):
         }
 
 
-def train_model(csv, pth, metadata, learning_rate=0.001, window_size=5):
+def train_model(pth, metadata, learning_rate=0.001):
     """"""
 
     with open(metadata, 'r') as f:
         meta = json.load(f)
 
+    chunk_size = meta['chunk_size']
+    chunks_per_file = meta['chunks_per_file']
+
     features = [c for c, s in zip(meta['column_order'], meta['scaling_status']) if s == 'scaled']
     feature_len = len(features)
-    df = pd.read_csv(csv, index_col='fid')
-    np.random.seed(1234)
-    fids = [f.split('.')[0] for f in os.listdir(pth)]
-    df = df.loc[fids].sample(frac=1)
-    df['rand'] = np.random.rand(len(df))
 
-    train = df.loc[df['rand'] < 0.8].index
-    train_files = [os.path.join(pth, '{}.pth'.format(f)) for f in train]
-    train_dataset = PTHLSTMDataset(train_files, window_size=window_size)
+    train_dir = os.path.join(pth, 'train')
+    train_files = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
+    train_dataset = PTHLSTMDataset(train_files, chunks_per_file=chunks_per_file)
     train_dataloader = DataLoader(train_dataset,
-                                  batch_size=64,
+                                  batch_size=256,
                                   shuffle=True,
                                   num_workers=6,
                                   collate_fn=lambda batch: [x for x in batch if x is not None])
 
-    val = df.loc[df['rand'] >= 0.8].index
-    val_files = [os.path.join(pth, '{}.pth'.format(f)) for f in val]
-    val_dataset = PTHLSTMDataset(val_files, window_size=window_size)
-    val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=6)
+    val_dir = os.path.join(pth, 'val')
+    val_files = [os.path.join(val_dir, f) for f in os.listdir(val_dir)]
+    val_dataset = PTHLSTMDataset(val_files, chunks_per_file=chunks_per_file)
+    val_dataloader = DataLoader(val_dataset, batch_size=256, shuffle=False, num_workers=6)
 
     model = LSTMPredictor(num_bands=feature_len, learning_rate=learning_rate)
 
@@ -155,5 +173,5 @@ if __name__ == '__main__':
     pth_ = os.path.join(d, 'training', target_var, 'scaled_pth')
     metadata_ = os.path.join(d, 'training', target_var, 'training_metadata.json')
 
-    train_model(fields, pth_, metadata_)
+    train_model(pth_, metadata_)
 # ========================= EOF ====================================================================
--- prep/build_training_sequences.py
+++ prep/build_training_sequences.py
@@ -1,15 +1,17 @@
 import os
 import json
+import random
 
 import numpy as np
 import pandas as pd
 import torch
+from sklearn.metrics import r2_score, root_mean_squared_error
 
 TERRAIN_FEATURES = ['slope', 'aspect', 'elevation', 'tpi_1250', 'tpi_250', 'tpi_150']
 
 
 def join_training(stations, ts_dir, rs_file, dem_dir, out_dir, scaling_json, var='rsds'):
-    """"""
+    ''''''
 
     stations = pd.read_csv(stations)
     stations['fid'] = [f.strip() for f in stations['fid']]
@@ -24,7 +26,7 @@ def join_training(stations, ts_dir, rs_file, dem_dir, out_dir, scaling_json, var
     if var == 'rsds':
         features = TERRAIN_FEATURES
     elif var == 'vpd':
-        features = TERRAIN_FEATURES + get_sr_features(rs_df)
+        features = TERRAIN_FEATURES
 
     features = ['fid'] + features
     rs_df = rs_df[features]
@@ -32,6 +34,8 @@ def join_training(stations, ts_dir, rs_file, dem_dir, out_dir, scaling_json, var
     rs_df.drop(columns=['fid'], inplace=True)
 
     ts, ct, scaling, first, shape = None, 0, {}, True, None
+
+    scaling['stations'] = []
 
     for tile in stations['MGRS_TILE'].unique():
         sol_file = os.path.join(dem_dir, 'tile_{}.csv'.format(tile))
@@ -47,7 +51,11 @@ def join_training(stations, ts_dir, rs_file, dem_dir, out_dir, scaling_json, var
             ts = pd.read_csv(sta_file, index_col='Unnamed: 0', parse_dates=True)
             rs = rs_df.loc[f].values
             sol = sol_df[f].to_dict()
-            ts = ts[[f'{var}_obs', f'{var}_gm'] + [c for c in ts.columns if c.endswith('_nl')]]
+
+            # training depends on having the first three columns like so
+            feats = [c for c in ts.columns if c.endswith('_nl') and var not in c]
+            ts = ts[[f'{var}_obs', f'{var}_gm', f'{var}_nl'] + feats]
+
             ts['doy'] = ts.index.dayofyear
             ts['rsun'] = ts['doy'].map(sol) * 0.0036
             ts[rs_df.columns] = np.ones((ts.shape[0], len(rs_df.columns))) * rs
@@ -59,24 +67,24 @@ def join_training(stations, ts_dir, rs_file, dem_dir, out_dir, scaling_json, var
                 post_ = ts.shape[0]
                 removed_nan = True
 
-            if scaling_json:
-                for c in ts.columns:
+            for c in ts.columns:
 
-                    for mm in ['max', 'min']:
+                for mm in ['max', 'min']:
 
-                        p = '{}_{}'.format(c, mm)
-                        v = ts[c].agg(mm, axis=0)
+                    p = '{}_{}'.format(c, mm)
+                    v = ts[c].agg(mm, axis=0)
 
-                        if p not in scaling.keys():
-                            scaling[p] = v.item()
+                    if p not in scaling.keys():
+                        scaling[p] = v.item()
 
-                        if mm == 'max' and v > scaling[p]:
-                            scaling[p] = v.item()
+                    if mm == 'max' and v > scaling[p]:
+                        scaling[p] = v.item()
 
-                        if mm == 'min' and v < scaling[p]:
-                            scaling[p] = v.item()
+                    if mm == 'min' and v < scaling[p]:
+                        scaling[p] = v.item()
 
-            outfile = os.path.join(out_dir, '{}.csv'.format(f))
+            # mark consecutive days
+            ts['doy_diff'] = ts.index.to_series().diff().dt.days.fillna(1)
 
             if first:
                 shape = ts.shape[1]
@@ -86,20 +94,37 @@ def join_training(stations, ts_dir, rs_file, dem_dir, out_dir, scaling_json, var
                     print('{} has {} cols, should have {}, skipped it'.format(f, ts.shape[1], shape))
                     continue
 
+            outfile = os.path.join(out_dir, '{}.csv'.format(f))
             # write csv without dt index
             ts.to_csv(outfile, index=False)
+            scaling['stations'].append(f)
+
             if removed_nan:
                 print(f, ts.shape[0], '{} rows with NaN'.format(pre_ - post_))
             else:
                 print(f, ts.shape[0])
 
-    if scaling_json:
-        with open(scaling_json, 'w') as fp:
-            json.dump(scaling, fp, indent=4)
+    with open(scaling_json, 'w') as fp:
+        json.dump(scaling, fp, indent=4)
+
+
+def print_rsmse(o, n, g):
+    r2_nl = r2_score(o, n)
+    rmse_nl = root_mean_squared_error(o, n)
+    print("r2_nldas", r2_nl)
+    print('rmse_nldas', rmse_nl)
+
+    r2_gm = r2_score(o, g)
+    rmse_gm = root_mean_squared_error(o, g)
+    print("r2_gridmet", r2_gm)
+    print('rmse_gridmet', rmse_gm)
+
+    print('{} records'.format(len(o)))
 
 
 def get_sr_features(df):
-    feats = [c for c in df.columns if c.startswith('B')]
+    feats = []
+    feats += [c for c in df.columns if c.startswith('B')]
     feats += [c for c in df.columns if c.startswith('evi')]
     feats += [c for c in df.columns if c.startswith('gi')]
     feats += [c for c in df.columns if c.startswith('nd')]
@@ -107,56 +132,109 @@ def get_sr_features(df):
     return feats
 
 
-def apply_scaling_and_save(csv_dir, scaling_json, training_metadata, output_dir,
-                           target='rsds_obs', comparison='rsds_gm'):
+def apply_scaling_and_save(csv_dir, scaling_json, training_metadata, output_dir, train_frac=0.8,
+                           chunk_size=100, chunks_per_file=1, target='rsds'):
     with open(scaling_json, 'r') as f:
         scaling_data = json.load(f)
 
     scaling_data['column_order'] = []
     scaling_data['scaling_status'] = []
     scaling_data['observation_count'] = 0
-    scaling_data['stations'] = []
 
+    padded_data = []
+    file_count = 0
     first = True
-    for filename in os.listdir(csv_dir):
-        if filename.endswith(".csv"):
+    fate = None
 
-            scaling_data['stations'].append(filename.split('.csv')[0])
+    stations = scaling_data['stations']
+    scaling_data['train_stations'] = []
+    scaling_data['val_stations'] = []
+    scaling_data['chunk_size'] = chunk_size
+    scaling_data['chunks_per_file'] = chunks_per_file
 
-            filepath = os.path.join(csv_dir, filename)
-            df = pd.read_csv(filepath)
+    if not os.path.exists(os.path.join(output_dir, 'val')):
+        os.mkdir(os.path.join(output_dir, 'val'))
 
-            if df.empty:
-                print('{} is empty'.format(filename))
+    if not os.path.exists(os.path.join(output_dir, 'train')):
+        os.mkdir(os.path.join(output_dir, 'train'))
+
+    destiny = ['train' if random.random() < train_frac else 'val' for _ in stations]
+
+    obs, gm, nl = [], [], []
+
+    for fate, station in zip(destiny, stations):
+
+        print(station)
+
+        scaling_data['{}_stations'.format(fate)].append(station)
+
+        filepath = os.path.join(csv_dir, '{}.csv'.format(station))
+        df = pd.read_csv(filepath)
+
+        if df.empty:
+            print('{} is empty'.format(station))
+            continue
+
+        if fate == 'val':
+            obs.extend(df[f'{target}_obs'].to_list())
+            gm.extend(df[f'{target}_gm'].to_list())
+            nl.extend(df[f'{target}_nl'].to_list())
+
+        day_diff = df['doy_diff'].astype(int).to_list()
+        df.drop(columns=['doy_diff'], inplace=True)
+
+        for col in df.columns:
+
+            if col in [f'{target}_obs', f'{target}_gm', 'doy_diff']:
+                if first:
+                    scaling_data['column_order'].append(col)
+                    scaling_data['scaling_status'].append('unscaled')
+                continue
+            else:
+                min_val = scaling_data[f'{col}_min']
+                max_val = scaling_data[f'{col}_max']
+                df[col] = (df[col] - min_val) / (max_val - min_val)
+                if first:
+                    scaling_data['column_order'].append(col)
+                    scaling_data['scaling_status'].append('scaled')
+
+        first = False
+
+        data_tensor = torch.tensor(df.values, dtype=torch.float32)
+        num_chunks = len(data_tensor) // chunk_size
+
+        for i in range(num_chunks):
+
+            chunk = data_tensor[i * chunk_size: (i + 1) * chunk_size]
+
+            consecutive = np.array(day_diff[i * chunk_size: (i + 1) * chunk_size])
+            sequence_check = np.all(consecutive == 1)
+            if not sequence_check:
                 continue
 
-            for col in df.columns:
+            if len(chunk) < chunk_size:
+                padding = torch.full((chunk_size - len(chunk), chunk.shape[1]), fill_value=float('nan'))
+                chunk = torch.cat([chunk, padding], dim=0)
+            padded_data.append(chunk)
 
-                if col in [target, comparison]:
-                    if first:
-                        scaling_data['column_order'].append(col)
-                        scaling_data['scaling_status'].append('unscaled')
-                    continue
-                else:
-                    min_val = scaling_data[f"{col}_min"]
-                    max_val = scaling_data[f"{col}_max"]
-                    df[col] = (df[col] - min_val) / (max_val - min_val)
-                    if first:
-                        scaling_data['column_order'].append(col)
-                        scaling_data['scaling_status'].append('scaled')
+            if len(padded_data) == chunks_per_file:
+                outfile = os.path.join(output_dir, fate, f'data_chunk_{file_count}.pth')
+                torch.save(torch.stack(padded_data), outfile)
+                scaling_data['observation_count'] += chunk_size * len(padded_data)
+                file_count += 1
+                # print(os.path.basename(outfile))
+                padded_data = []
 
-            first = False
-
-            scaling_data['observation_count'] += df.shape[0]
-            data_tensor = torch.tensor(df.values, dtype=torch.float32)
-            outfile = os.path.join(output_dir, os.path.splitext(filename)[0] + '.pth')
-            torch.save(data_tensor, outfile)
-            print(os.path.basename(outfile))
+    if padded_data:
+        outfile = os.path.join(output_dir, fate, f'data_chunk_{file_count}.pth')
+        torch.save(torch.stack(padded_data), outfile)
+        scaling_data['observation_count'] += chunk_size * len(padded_data)
 
     with open(training_metadata, 'w') as fp:
         json.dump(scaling_data, fp, indent=4)
 
     print('{} sites; {} observations'.format(len(scaling_data['stations']), scaling_data['observation_count']))
+    print_rsmse(obs, nl, gm)
 
 
 if __name__ == '__main__':
@@ -172,13 +250,17 @@ if __name__ == '__main__':
     rs = os.path.join(d, 'rs', 'dads_stations', 'landsat', 'dads_stations_WMT_500_2023.csv')
     solrad = os.path.join(d, 'dem', 'rsun_tables')
     out_csv = os.path.join(d, 'training', target_var, 'compiled_csv')
-    scaling_ = os.path.join(d, 'training', target_var, 'scaling.json')
+    if not os.path.exists(out_csv):
+        os.mkdir(out_csv)
+    scaling_ = os.path.join(d, 'training', target_var, 'scaling_metadata.json')
 
     join_training(fields, sta, rs, solrad, out_csv, scaling_json=scaling_, var=target_var)
 
     metadata = os.path.join(d, 'training', target_var, 'training_metadata.json')
     out_pth = os.path.join(d, 'training', target_var, 'scaled_pth')
 
-    target_col, comparison_col = f'{target_var}_obs', f'{target_var}_gm'
-    apply_scaling_and_save(out_csv, scaling_, metadata, out_pth, target=target_col, comparison=comparison_col)
+    if not os.path.exists(out_pth):
+        os.mkdir(out_pth)
+
+    apply_scaling_and_save(out_csv, scaling_, metadata, out_pth, target=target_var)
 # ========================= EOF ==============================================================================
