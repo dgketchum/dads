fd330c4431a2d24fe21718d6240ae3c6dae3944f
dgketchum <dgketchum@gmail.com>
2024-08-16 10:51:14 -0600

    cleanup non-unique FID

--- process/stations/join_station_lists.py
+++ process/stations/join_station_lists.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 import requests
 import numpy as np
@@ -23,6 +24,10 @@ def join_stations(snotel, mesonet, agrimet, out_file, fill_elevation=False, boun
                                                 'ELEV_FT': 'elevation',
                                                 'STATION_LAT': 'latitude',
                                                 'STATION_LON': 'longitude'})
+    agrimet_data['fid'] = agrimet_data['fid'].astype(str)
+    agrimet_data['fid'] = [s.upper() for s in agrimet_data['fid']]
+    agrimet_data['name'] = agrimet_data['name'].astype(str)
+    agrimet_data['name'] = [s.upper() for s in agrimet_data['name']]
 
     mesonet_data = mesonet_data.rename(columns={'index': 'fid', 'NAME': 'name', 'ELEV': 'elevation'})
     mesonet_data['fid'] = mesonet_data['fid'].astype(str)
@@ -38,55 +43,45 @@ def join_stations(snotel, mesonet, agrimet, out_file, fill_elevation=False, boun
 
     snotel_data = snotel_data[['fid', 'name', 'elevation', 'latitude', 'longitude', 'source']]
     snotel_data['fid'] = [f.strip() for f in snotel_data['fid']]
+    snotel_data = snotel_data.drop_duplicates()
+    d, suspects = {}, []
+    add_records(d, snotel_data.to_dict(orient='index'), suspects)
+
     mesonet_data = mesonet_data[['fid', 'name', 'elevation', 'latitude', 'longitude', 'source']]
+    add_records(d, mesonet_data.to_dict(orient='index'), suspects)
+
     agrimet_data = agrimet_data[['fid', 'name', 'elevation', 'latitude', 'longitude', 'source']]
+    add_records(d, agrimet_data.to_dict(orient='index'), suspects)
 
-    agrimet_names = [f.lower() for f in agrimet_data['name']]
-    dup_gwx_madis = [r['fid'] for i, r in mesonet_data.iterrows() if r['fid'].lower().strip() in agrimet_names]
-    idx = [i for i, r in mesonet_data['fid'].items() if r not in dup_gwx_madis]
-    mesonet_data = mesonet_data.loc[idx]
 
-    comb_df = pd.concat([snotel_data, mesonet_data, agrimet_data])
-
-    for i, r in comb_df.iterrows():
-        try:
-            fid = int(r['fid'])
-            if 'gwx' in r['source']:
-                comb_df.loc[i, 'fid'] = '{}_gwx'.format(r['fid']).upper()
-            else:
-                comb_df.loc[i, 'fid'] = '{}_{}'.format(r['fid'], r['source']).upper()
-        except ValueError:
-            comb_df.loc[i, 'fid'] = r['fid'].upper()
-            continue
-
-    if bounds:
-        w, s, e, n = bounds
-        comb_df = comb_df[(comb_df['latitude'] < n) & (comb_df['latitude'] >= s)]
-        comb_df = comb_df[(comb_df['longitude'] < e) & (comb_df['longitude'] >= w)]
 
     if fill_elevation:
-        for i, r in comb_df.iterrows():
+        for e, (i, r) in enumerate(d.items(), start=1):
             try:
-                if r['elevation'] > 3000.0 or r['elevation'] < 0.0:
-                    comb_df.loc[i, 'elevation'] = elevation_from_coordinate(r['latitude'], r['longitude'])
-                    print(r['fid'], i, 'of', comb_df.shape[0])
+                if r['elevation'] > 4200.0 or r['elevation'] < 0.0:
+                    d[i]['elevation'] = elevation_from_coordinate(r['latitude'], r['longitude'])
+                    print(i, r['source'], e, 'of', len(d))
                 if isinstance(r['elevation'], type(None)):
-                    comb_df.loc[i, 'elevation'] = elevation_from_coordinate(r['latitude'], r['longitude'])
-                    print(r['fid'], i, 'of', comb_df.shape[0])
+                    d[i]['elevation'] = elevation_from_coordinate(r['latitude'], r['longitude'])
+                    print(i, r['source'], e, 'of', len(d))
                 elif np.isnan(r['elevation']):
-                    comb_df.loc[i, 'elevation'] = elevation_from_coordinate(r['latitude'], r['longitude'])
-                    print(r['fid'], i, 'of', comb_df.shape[0])
+                    d[i]['elevation'] = elevation_from_coordinate(r['latitude'], r['longitude'])
+                    print(i, r['source'], e, 'of', len(d))
                 else:
                     pass
             except KeyError:
-                print('Elevation error at {}'.format(r['fid']))
+                print('Elevation error at {}'.format(i))
                 continue
             except requests.exceptions.JSONDecodeError:
-                print('Elevation error at {}'.format(r['fid']))
+                print('Elevation error at {}'.format(i))
                 continue
             except Exception as e:
-                print('Elevation error {} at {}'.format(e, r['fid']))
+                print('Elevation error {} at {}'.format(e, i))
                 continue
+
+    comb_df = pd.DataFrame.from_dict(d, orient='index')
+    comb_df = comb_df.sort_index()
+    comb_df['fid'] = comb_df.index
 
     geometry = [Point(xy) for xy in zip(comb_df['longitude'], comb_df['latitude'])]
     gdf = gpd.GeoDataFrame(comb_df, geometry=geometry)
@@ -98,22 +93,43 @@ def join_stations(snotel, mesonet, agrimet, out_file, fill_elevation=False, boun
     print(out_file)
 
 
+def add_records(d, n, l):
+
+    for k, v in n.items():
+
+        try:
+            _ = int(v['fid'])
+            int_name = True
+        except ValueError:
+            int_name = False
+
+        if int_name:
+            r = re.sub(r'\W+', '_', v['name'])[:10].upper()
+            d[r] = {kk: vv for kk, vv in v.items() if kk != 'fid'}
+            continue
+
+
+        if v['fid'] not in d.keys():
+            d[v['fid']] = {kk: vv for kk, vv in v.items() if kk != 'fid'}
+        elif v['name'] == d[v['fid']]['name']:
+            continue
+        elif d[v['fid']]['latitude'] - v['latitude'] < 0.2:
+            continue
+        else:
+            r = re.sub(r'\W+', '_', v['name'])[:10].upper()
+            d[r] = {kk: vv for kk, vv in v.items() if kk != 'fid'}
+            l.append(v['fid'])
+
+
 if __name__ == '__main__':
     d = '/media/research/IrrigationGIS'
     if not os.path.exists(d):
         d = '/home/dgketchum/data/IrrigationGIS'
+
     sno_list = os.path.join(d, 'climate', 'snotel', 'snotel_list.csv')
     meso_list = os.path.join(d, 'climate', 'madis', 'mesonet_sites.csv')
     agrim_list = os.path.join(d, 'dads', 'met', 'stations', 'openet_gridwxcomp_input.csv')
-
-    stations = os.path.join(d, 'dads', 'met', 'stations', 'dads_stations_renamed.shp')
+    stations = os.path.join(d, 'dads', 'met', 'stations', 'dads_stations_renamed_elev.shp')
     join_stations(sno_list, meso_list, agrim_list, stations, bounds=None, fill_elevation=True)
-
-    station_elev = os.path.join(d, 'dads', 'met', 'stations', 'dads_stations_elev_mgrs_named.shp')
-    # fix_names(stations, station_elev)
-
-    sites = os.path.join(d, 'dads', 'met', 'stations', 'dads_stations_elev_mgrs.csv')
-    grid_dir = os.path.join(d, 'dads', 'met', 'gridded')
-    # cleanup_gwx_misname(grid_dir, sites)
 
 # ========================= EOF ====================================================================
