76ec39c2d132bbf8949c754d4e06922fb4685496
dgketchum <dgketchum@gmail.com>
2024-08-19 17:19:26 -0600

    write under new unique naming

--- process/obs/gwx.py
+++ process/obs/gwx.py
@@ -1,6 +1,8 @@
 import os
+import re
 
 import pandas as pd
+from pandarallel import pandarallel
 
 from utils.calc_eto import calc_asce_params
 from utils.station_parameters import station_par_map
@@ -18,19 +20,28 @@ RENAME = {
 }
 
 
-def read_gwx(stations, gwx_src, gwx_dst, overwrite=False):
+def read_gwx(stations, target_sites, gwx_src, gwx_dst, overwrite=False):
     """"""
-    sites = pd.read_csv(stations)
+    network_sites = pd.read_csv(stations)
+    dads_sites = pd.read_csv(target_sites, index_col='index')
 
     kw = station_par_map('openet')
 
-    for i, row in sites.iterrows():
+    for i, row in network_sites.iterrows():
 
+        # handle non-unique station names as in join_station_lists
         try:
-            fid = int(row['original_network_id'])
-            base_name = '{}_gwx'.format(row['original_network_id']).upper()
+            _ = int(row['original_network_id'])
+            int_name = True
         except ValueError:
+            int_name = False
+
+        if int_name:
+            base_name = re.sub(r'\W+', '_', row['original_station_name'])[:10].upper()
+        else:
             base_name = row['original_network_id'].upper()
+
+        assert base_name in dads_sites.index
 
         out_file = os.path.join(gwx_dst, '{}.csv'.format(base_name))
 
@@ -49,10 +60,10 @@ def read_gwx(stations, gwx_src, gwx_dst, overwrite=False):
         df = pd.read_excel(in_file, index_col=0)
         df['date'] = df.index
         df.rename(columns=RENAME, inplace=True)
-        df = df[[v for k, v in RENAME.items()]]
+        df = df[[v for k, v in RENAME.items()]].copy()
         df['rsds'] *= 0.0864
-        # asce_params = daily_df.parallel_apply(calc_asce_params, lat=lat_, elev=elev_, zw=10, axis=1)
-        asce_params = df.apply(calc_asce_params, lat=lat, elev=elv, zw=zw, axis=1)
+        asce_params = df.parallel_apply(calc_asce_params, lat=lat, elev=elv, zw=zw, axis=1)
+        # asce_params = df.apply(calc_asce_params, lat=lat, elev=elv, zw=zw, axis=1)
 
         try:
             df[['mean_temp', 'vpd', 'rn', 'u2', 'eto']] = pd.DataFrame(asce_params.tolist(),
@@ -71,14 +82,14 @@ if __name__ == '__main__':
         home = os.path.expanduser('~')
         d = os.path.join(home, 'data', 'IrrigationGIS')
 
-    # pandarallel.initialize(nb_workers=6)
+    pandarallel.initialize(nb_workers=6)
 
-    fields = os.path.join(d, 'dads', 'met', 'stations', 'dads_stations_elev_mgrs_named.csv')
+    dads_stations = os.path.join(d, 'dads', 'met', 'stations', 'dads_stations_elev_mgrs.csv')
     gwx_list = os.path.join(d, 'dads', 'met', 'stations', 'openet_gridwxcomp_input.csv')
 
     gwx_src_ = os.path.join(d, 'climate', 'gridwxcomp', 'station_data')
     gwx_dst_ = os.path.join(d, 'dads', 'met', 'obs', 'gwx')
 
-    read_gwx(gwx_list, gwx_src_, gwx_dst_, overwrite=False)
+    read_gwx(gwx_list, dads_stations, gwx_src_, gwx_dst_, overwrite=False)
 
 # ========================= EOF ====================================================================
