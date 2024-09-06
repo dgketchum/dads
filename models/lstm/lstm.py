38c7f2cc7137d500f7c75d9cc48f899d1662ba6a
dgketchum <dgketchum@gmail.com>
2024-09-06 11:18:34 -0600

    tsl datetime encoding functionality

--- models/lstm/lstm.py
+++ models/lstm/lstm.py
@@ -1,84 +1,10 @@
-import os
 import csv
-import json
-import resource
-from datetime import datetime
 
 import pytorch_lightning as pl
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-
 from sklearn.metrics import r2_score, root_mean_squared_error
+from torch import nn as nn, optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.utils.data import DataLoader, Dataset
-from torch.utils.data._utils.collate import default_collate
-from pytorch_lightning.callbacks import Callback
-
-device_name = None
-if torch.cuda.is_available():
-    device_name = torch.cuda.get_device_name(0)
-    print(f'Using GPU: {device_name}')
-else:
-    print('CUDA is not available. PyTorch will use the CPU.')
-
-torch.set_float32_matmul_precision('medium')
-torch.cuda.get_device_name(torch.cuda.current_device())
-
-rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
-resource.setrlimit(resource.RLIMIT_NOFILE, (4096, rlimit[1]))
-
-
-class PTHLSTMDataset(Dataset):
-    def __init__(self, file_paths, col_index, expected_width, chunk_size, transform=None):
-        self.chunk_size = chunk_size
-        self.transform = transform
-        self.col_index = col_index
-
-        all_data = []
-        for file_path in file_paths:
-            data = torch.load(file_path, weights_only=True)
-            if data.shape[2] != expected_width:
-                print(f"Skipping {file_path},shape mismatch. Expected {expected_width} columns, got {data.shape[2]}")
-                continue
-            all_data.append(data)
-
-        self.data = torch.cat(all_data, dim=0)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        chunk = self.data[idx]
-
-        valid_rows = ~torch.isnan(chunk).any(dim=1)
-        chunk = chunk[valid_rows]
-
-        if len(chunk) < self.chunk_size:
-            return None
-
-        y, gm, lf, hf = (chunk[:, self.col_index[0]],
-                         chunk[:, self.col_index[1]],
-                         chunk[:, self.col_index[2]: self.col_index[3]],
-                         chunk[:, self.col_index[3]:])
-
-        return y, gm, lf, hf
-
-
-def custom_collate(batch):
-    batch = list(filter(lambda x: x is not None, batch))
-    if not batch:
-        return None
-    return default_collate(batch)
-
-
-def stack_batch(batch):
-    y = torch.stack([item[0] for item in batch])
-    gm = torch.stack([item[1] for item in batch])
-    lf = torch.stack([item[2] for item in batch])
-    hf = torch.stack([item[3] for item in batch])
-    return y, gm, lf, hf
 
 
 class LSTMPredictor(pl.LightningModule):
@@ -248,112 +174,14 @@ class LSTMPredictor(pl.LightningModule):
         }
 
 
-def train_model(dirpath, pth, metadata, batch_size=1, learning_rate=0.01, n_workers=1, logging_csv=None):
-    """"""
-
-    with open(metadata, 'r') as f:
-        meta = json.load(f)
-
-    data_frequency = meta['data_frequency']
-    # idxs: obs, gm, start_daily: end_daily, start_hourly:
-    hf_idx = data_frequency.index('hf')
-    idxs = (0, 1, 2, hf_idx)
-    hf_bands = data_frequency.count('hf')
-    lf_bands = data_frequency.count('lf') - 2
-    tensor_width = data_frequency.count('lf') + data_frequency.count('hf')
-    print('tensor cols: {}'.format(tensor_width))
-    chunk_size = meta['chunk_size']
-
-    model = LSTMPredictor(num_bands_lf=lf_bands,
-                          num_bands_hf=hf_bands,
-                          learning_rate=learning_rate,
-                          log_csv=logging_csv)
-
-    tdir = os.path.join(pth, 'train')
-    t_files = [os.path.join(tdir, f) for f in os.listdir(tdir)]
-    train_dataset = PTHLSTMDataset(file_paths=t_files,
-                                   col_index=idxs,
-                                   expected_width=tensor_width,
-                                   chunk_size=chunk_size)
-
-    train_dataloader = DataLoader(train_dataset,
-                                  batch_size=batch_size,
-                                  shuffle=True,
-                                  num_workers=n_workers,
-                                  collate_fn=lambda batch: [x for x in batch if x is not None])
-
-    vdir = os.path.join(pth, 'val')
-    v_files = [os.path.join(vdir, f) for f in os.listdir(vdir)]
-    val_dataset = PTHLSTMDataset(file_paths=v_files,
-                                 col_index=idxs,
-                                 expected_width=tensor_width,
-                                 chunk_size=chunk_size)
-
-    val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=n_workers,
-                                collate_fn=custom_collate)
-
-    print(f"Number of training samples: {len(train_dataset)}")
-    print(f"Number of validation samples: {len(val_dataset)}")
-
-    checkpoint_callback = ModelCheckpoint(
-        monitor="val_loss",
-        filename="best_model",
-        dirpath=dirpath,
-        save_top_k=1,
-        mode="min"
-    )
-
-    early_stop_callback = EarlyStopping(
-        monitor="val_loss",
-        min_delta=0.00,
-        patience=100,
-        verbose=False,
-        mode="min",
-        check_finite=True,
-    )
-
-    trainer = pl.Trainer(max_epochs=1000, callbacks=[checkpoint_callback, early_stop_callback],
-                         accelerator='gpu', devices=1)
-    trainer.fit(model, train_dataloader, val_dataloader)
+def stack_batch(batch):
+    y = torch.stack([item[0] for item in batch])
+    gm = torch.stack([item[1] for item in batch])
+    lf = torch.stack([item[2] for item in batch])
+    hf = torch.stack([item[3] for item in batch])
+    return y, gm, lf, hf
 
 
 if __name__ == '__main__':
-
-    d = '/media/research/IrrigationGIS/dads'
-    if not os.path.exists(d):
-        d = '/home/dgketchum/data/IrrigationGIS/dads'
-
-    target_var = 'vpd'
-
-    if device_name == 'NVIDIA GeForce RTX 2080':
-        workers = 6
-    elif device_name == 'NVIDIA RTX A6000':
-        workers = 6
-    else:
-        raise NotImplementedError('Specify the machine this is running on')
-
-    zoran = '/home/dgketchum/training'
-    nvm = '/media/nvm/training'
-    if os.path.exists(zoran):
-        print('modeling with data from zoran')
-        training = zoran
-    elif os.path.exists(nvm):
-        print('modeling with data from NVM drive')
-        training = nvm
-    else:
-        print('modeling with data from UM drive')
-        training = os.path.join(d, 'training')
-
-    print('========================== modeling {} =========================='.format(target_var))
-
-    param_dir = os.path.join(training, target_var)
-    pth_ = os.path.join(param_dir, 'scaled_pth')
-    metadata_ = os.path.join(param_dir, 'training_metadata.json')
-
-    now = datetime.now().strftime('%m%d%H%M')
-    chk = os.path.join(param_dir, 'checkpoints', now)
-    os.mkdir(chk)
-    logger_csv = os.path.join(chk, 'training_{}.csv'.format(now))
-
-    train_model(chk, pth_, metadata_, batch_size=512, learning_rate=0.001, n_workers=workers, logging_csv=logger_csv)
+    pass
 # ========================= EOF ====================================================================
--- models/lstm/write_training_data.py
+++ models/lstm/write_training_data.py
@@ -1,16 +1,14 @@
 import os
 import json
 import random
-from datetime import datetime, timedelta
+from datetime import datetime
 
-from tqdm import tqdm
+import torch
 import numpy as np
 import pandas as pd
-import torch
-
 from sklearn.metrics import r2_score, root_mean_squared_error
 
-from process.rs.landsat_prep import build_landsat_tables
+from models.dt_encoder import datetime_encoded
 
 TERRAIN_FEATURES = ['slope', 'aspect', 'elevation', 'tpi_1250', 'tpi_250', 'tpi_150']
 
@@ -27,17 +25,15 @@ def print_rmse(o, n, g):
     print('rmse_gridmet', rmse_gm)
 
 
-def apply_scaling_and_save(csv_dir, scaling_json, training_metadata, output_dir, train_frac=0.8, chunk_size=72,
+def apply_scaling_and_save(csv_dir, training_metadata, output_dir, train_frac=0.8, chunk_size=72,
                            chunks_per_file=1000, target='rsds', hourly_dir=None, shuffle=False):
-    with open(scaling_json, 'r') as f:
-        scaling_data = json.load(f)
 
-    scaling_data['chunk_size'] = chunk_size
-    scaling_data['chunks_per_file'] = chunks_per_file
-    scaling_data['column_order'] = []
-    scaling_data['scaling_status'] = []
-    scaling_data['data_frequency'] = []
-    scaling_data['observation_count'] = 0
+    metadata = {}
+    metadata['chunk_size'] = chunk_size
+    metadata['chunks_per_file'] = chunks_per_file
+    metadata['column_order'] = []
+    metadata['data_frequency'] = []
+    metadata['observation_count'] = 0
 
     files_ = [f for f in os.listdir(csv_dir) if f.endswith('.csv')]
 
@@ -53,9 +49,6 @@ def apply_scaling_and_save(csv_dir, scaling_json, training_metadata, output_dir,
     first, write_files = True, 0
     for j, (fate, station) in enumerate(zip(destiny, stations), start=1):
 
-        # if station != 'LWAO2':
-        #     continue
-
         v_file = os.path.join(output_dir, 'train', '{}.pth'.format(station))
         t_file = os.path.join(output_dir, 'val', '{}.pth'.format(station))
 
@@ -66,6 +59,9 @@ def apply_scaling_and_save(csv_dir, scaling_json, training_metadata, output_dir,
         filepath = os.path.join(csv_dir, '{}.csv'.format(station))
         try:
             df = pd.read_csv(filepath, index_col=0, parse_dates=True)
+            yr_dt_encoding = datetime_encoded(df, ['year'])
+            df = pd.concat([df, yr_dt_encoding], ignore_index=False, axis=1)
+
             if hourly_dir:
                 hr_filepath = os.path.join(hourly_dir, '{}.csv'.format(station))
                 dfhr = pd.read_csv(hr_filepath, index_col=0, parse_dates=True)
@@ -75,6 +71,9 @@ def apply_scaling_and_save(csv_dir, scaling_json, training_metadata, output_dir,
                 end_date = min(df.index.max(), dfhr.index.max())
                 df = df.loc[start_date:end_date]
                 dfhr = dfhr.loc[start_date:end_date]
+
+                day_dt_encoding = datetime_encoded(dfhr, ['day'])
+                dfhr = pd.concat([dfhr, day_dt_encoding], ignore_index=False, axis=1)
 
         except FileNotFoundError:
             continue
@@ -89,38 +88,14 @@ def apply_scaling_and_save(csv_dir, scaling_json, training_metadata, output_dir,
             nl.extend(df[f'{target}_nl'].to_list())
 
         day_diff = df['dt_diff'].astype(int).to_list()
-        df.drop(columns=['dt_diff'], inplace=True)
+        df.drop(columns=['dt_diff', 'doy'], inplace=True)
+        dfhr.drop(columns=['doy', 'hour'], inplace=True)
 
-        for col in df.columns:
+        metadata['column_order'] = df.columns.to_list()
+        metadata['column_order'].extend(dfhr.columns.to_list())
 
-            if col in [f'{target}_obs', f'{target}_gm']:
-                if first:
-                    scaling_data['column_order'].append(col)
-                    scaling_data['scaling_status'].append('unscaled')
-                    scaling_data['data_frequency'].append('lf')
-                continue
-            else:
-                min_val = scaling_data[f'{col}_min']
-                max_val = scaling_data[f'{col}_max']
-                df[col] = (df[col] - min_val) / (max_val - min_val)
-                if first:
-                    scaling_data['column_order'].append(col)
-                    scaling_data['scaling_status'].append('scaled')
-                    scaling_data['data_frequency'].append('lf')
-
-        for col in dfhr.columns:
-            if col == 'doy':
-                min_val, max_val = 0, 365
-            else:
-                min_val = scaling_data[f'{col}_nl_hr_min']
-                max_val = scaling_data[f'{col}_nl_hr_max']
-            dfhr[col] = (dfhr[col] - min_val) / (max_val - min_val)
-            if first:
-                scaling_data['column_order'].append(col)
-                scaling_data['scaling_status'].append('scaled')
-                scaling_data['data_frequency'].append('hf')
-
-        first = False
+        [metadata['data_frequency'].append('lf') for _ in df.columns]
+        [metadata['data_frequency'].append('hf') for _ in dfhr.columns]
 
         data_tensor_daily = torch.tensor(df.values, dtype=torch.float32)
         data_tensor_hourly = torch.tensor(dfhr.values, dtype=torch.float32)
@@ -159,16 +134,18 @@ def apply_scaling_and_save(csv_dir, scaling_json, training_metadata, output_dir,
             write_files += 1
             print('{}; {} of {} to {}, {} chunks, size {}'.format(station, j, len(stations), fate,
                                                                   station_chunk_ct, stack.shape))
-            scaling_data['observation_count'] += chunk_size * len(station_chunks)
+            metadata['observation_count'] += chunk_size * len(station_chunks)
 
     if training_metadata:
         with open(training_metadata, 'w') as fp:
-            json.dump(scaling_data, fp, indent=4)
+            json.dump(metadata, fp, indent=4)
 
     print('\n{} sites\n{}; {} observations, {} held out for validation'.format(write_files, target_var,
-                                                                               scaling_data['observation_count'],
+                                                                               metadata['observation_count'],
                                                                                len(obs)))
-    print_rmse(obs, nl, gm)
+
+    if len(obs) > 0:
+        print_rmse(obs, nl, gm)
 
 
 if __name__ == '__main__':
@@ -194,9 +171,7 @@ if __name__ == '__main__':
     param_dir = os.path.join(training, target_var)
 
     out_csv = os.path.join(param_dir, 'compiled_csv')
-    out_pth = os.path.join(param_dir, 'scaled_pth')
-
-    scaling_ = os.path.join(param_dir, 'scaling_metadata.json')
+    out_pth = os.path.join(param_dir, 'pth')
 
     # hourly_data = None
     hourly_data = os.path.join(d, 'met', 'gridded', 'nldas2_hourly')
@@ -210,10 +185,10 @@ if __name__ == '__main__':
     if not os.path.exists(os.path.join(out_pth, 'train')):
         os.mkdir(os.path.join(out_pth, 'train'))
 
-    print('========================== scaling {} =========================='.format(target_var))
+    print('========================== writing {} traing data =========================='.format(target_var))
 
-    # metadata = None
-    metadata = os.path.join(param_dir, 'training_metadata.json')
-    apply_scaling_and_save(out_csv, scaling_, metadata, out_pth, target=target_var, hourly_dir=hourly_data,
+    metadata = None
+    # metadata = os.path.join(param_dir, 'training_metadata.json')
+    apply_scaling_and_save(out_csv, metadata, out_pth, target=target_var, hourly_dir=hourly_data,
                            chunk_size=48, shuffle=True)
 # ========================= EOF ==============================================================================
