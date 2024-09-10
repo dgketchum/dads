e859025b4c95b99b00806cb446aec75fe6f00fe7
dgketchum <dgketchum@gmail.com>
2024-09-10 17:03:25 -0600

    simple function for ERA-5 downloads by year

--- extract/met_data/gridded_download.py
+++ extract/met_data/gridded_download.py
@@ -109,26 +109,31 @@ def gridmet_par_map():
     }
 
 
-def download_era5():
-    dataset = "reanalysis-era5-single-levels"
-    request = {
-        'product_type': ['reanalysis'],
-        'variable': ['10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature', '2m_temperature',
-                     'surface_pressure', 'total_precipitation', 'surface_net_solar_radiation'],
-        'year': ['2023'],
-        'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-        'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17',
-                '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
-        'time': ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00',
-                 '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00',
-                 '22:00', '23:00'],
-        'data_format': 'netcdf',
-        'download_format': 'zip',
-        'area': [53, -125, 23, -66]
-    }
+def download_era5(dst):
+    for y in range(2000, 2024):
+        dataset = "reanalysis-era5-single-levels"
+        request = {
+            'product_type': ['reanalysis'],
+            'variable': ['10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature', '2m_temperature',
+                         'surface_pressure', 'total_precipitation', 'surface_net_solar_radiation'],
+            'year': ['2023'],
+            'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+            'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17',
+                    '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
+            'time': ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00',
+                     '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00',
+                     '22:00', '23:00'],
+            'data_format': 'netcdf',
+            'download_format': 'zip',
+            'area': [53, -125, 23, -66]
+        }
 
-    client = cdsapi.Client()
-    client.retrieve(dataset, request).download()
+        client = cdsapi.Client()
+        result = client.retrieve(dataset, request)
+        target = os.path.join(dst, f'{y}.nc.gz')
+        result.download(target)
+        print(target)
+
 
 
 if __name__ == '__main__':
@@ -146,6 +151,7 @@ if __name__ == '__main__':
 
     # extract_met_data(sites, grid_dir, overwrite=False, station_type='dads',
     #                  shuffle=True, bounds=(-125., 25., -66., 49.), gridmet=True)
-    download_era5()
+    dest_ = os.path.join(grid_dir, 'era5', 'netCDF')
+    download_era5(dest_)
 
 # ========================= EOF ====================================================================
