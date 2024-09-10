980a74317dad56d5ae26b34a6d57a2b8c9e3468f
dgketchum <dgketchum@gmail.com>
2024-09-10 08:47:28 -0600

    add cdsapi code to download ERA-5

--- extract/met_data/gridded_download.py
+++ extract/met_data/gridded_download.py
@@ -2,6 +2,7 @@ import os
 
 import pandas as pd
 import pynldas2 as nld
+import cdsapi
 
 from extract.met_data.thredds import GridMet
 from utils.station_parameters import station_par_map
@@ -108,6 +109,28 @@ def gridmet_par_map():
     }
 
 
+def download_era5():
+    dataset = "reanalysis-era5-single-levels"
+    request = {
+        'product_type': ['reanalysis'],
+        'variable': ['10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature', '2m_temperature',
+                     'surface_pressure', 'total_precipitation', 'surface_net_solar_radiation'],
+        'year': ['2023'],
+        'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+        'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17',
+                '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
+        'time': ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00',
+                 '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00',
+                 '22:00', '23:00'],
+        'data_format': 'netcdf',
+        'download_format': 'zip',
+        'area': [53, -125, 23, -66]
+    }
+
+    client = cdsapi.Client()
+    client.retrieve(dataset, request).download()
+
+
 if __name__ == '__main__':
     d = '/media/research/IrrigationGIS'
     if not os.path.isdir(d):
@@ -121,7 +144,8 @@ if __name__ == '__main__':
 
     grid_dir = os.path.join(d, 'dads', 'met', 'gridded')
 
-    extract_met_data(sites, grid_dir, overwrite=False, station_type='dads',
-                     shuffle=True, bounds=(-125., 25., -66., 49.), gridmet=True)
+    # extract_met_data(sites, grid_dir, overwrite=False, station_type='dads',
+    #                  shuffle=True, bounds=(-125., 25., -66., 49.), gridmet=True)
+    download_era5()
 
 # ========================= EOF ====================================================================
