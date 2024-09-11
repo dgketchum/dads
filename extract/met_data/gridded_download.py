4152e6aed93a57118847d20bf84562d25b743f7e
dgketchum <dgketchum@gmail.com>
2024-09-11 10:03:48 -0600

    minor

--- extract/met_data/gridded_download.py
+++ extract/met_data/gridded_download.py
@@ -114,11 +114,13 @@ def download_era5(dst):
         dataset = "reanalysis-era5-single-levels"
         request = {
             'product_type': ['reanalysis'],
-            'variable': ['10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature', '2m_temperature',
+            'variable': ['10m_u_component_of_wind', '10m_v_component_of_wind', '2m_dewpoint_temperature',
+                         '2m_temperature',
                          'surface_pressure', 'total_precipitation', 'surface_net_solar_radiation'],
             'year': ['2023'],
             'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
-            'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17',
+            'day': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16',
+                    '17',
                     '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
             'time': ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', '10:00',
                      '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00',
@@ -133,7 +135,6 @@ def download_era5(dst):
         target = os.path.join(dst, f'{y}.nc.gz')
         result.download(target)
         print(target)
-
 
 
 if __name__ == '__main__':
