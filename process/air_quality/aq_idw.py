47741b8b8c4ba7e7302e218486270c23935178a2
dgketchum <dgketchum@gmail.com>
2024-09-13 16:49:17 -0600

    use dask for CONUS interpolation

--- process/air_quality/aq_idw.py
+++ process/air_quality/aq_idw.py
@@ -4,6 +4,10 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 from scipy.spatial import cKDTree
+import dask
+from dask.distributed import Client
+
+from extract.air_quality.aqepa import AQS_PARAMETERS
 
 
 def csv_to_netcdf(in_dir, metadata, output_file):
@@ -14,8 +18,6 @@ def csv_to_netcdf(in_dir, metadata, output_file):
     aqm.index = aqm.apply(lambda r: f"{str(r['st_fips']).rjust(2, '0')}"
                                     f"{str(r['co_fips']).rjust(3, '0')}"
                                     f"{str(r['site_no']).rjust(4, '0')}", axis=1)
-
-    # aqm = aqm[aqm['state'] == 'MT']
 
     files_ = [os.path.join(in_dir, f) for f in os.listdir(in_dir) if f.endswith('.csv')]
 
@@ -79,38 +81,74 @@ def interpolate_at_points(data, parameter, points, resolution=0.05, lat=(42.0, 4
 
 def interpolate_station_aq(nc_file, output, stations, raster_dst=None):
     """"""
-    params = ['pm2.5', 'pm10', 'no2', 'ozone', 'so2']
-    ds = xr.open_dataset(nc_file)
-    w, e = np.min(ds.longitude.values), np.max(ds.longitude.values)
-    s, n = np.min(ds.latitude.values), np.max(ds.latitude.values)
-
     stations = pd.read_csv(stations, index_col=0)
-    stations = stations[(stations['latitude'] < n) & (stations['latitude'] >= s)]
-    stations = stations[(stations['longitude'] < e) & (stations['longitude'] >= w)]
-    points = stations[['longitude', 'latitude']].values
-    results = {}
 
-    for day in ds.date.values:
+    params = ['pm2.5', 'pm10', 'no2', 'ozone', 'so2']
+    mds = xr.open_dataset(nc_file)
+    west, east = np.min(mds.longitude.values), np.max(mds.longitude.values)
+    south, north = np.min(mds.latitude.values), np.max(mds.latitude.values)
+
+    north = 49.0
+
+    stations = stations[(stations['latitude'] < north) & (stations['latitude'] >= south)]
+    stations = stations[(stations['longitude'] < east) & (stations['longitude'] >= west)]
+    points = stations[['longitude', 'latitude']]
+
+    client = Client()
+
+    tasks = []
+    for year in range(2000, 2025):
+        tasks.append(process_year(year, mds, points, params, south, north, west, east, output, raster_dst))
+
+    dask.compute(*tasks)
+    client.close()
+
+
+@dask.delayed
+def process_year(year, mds, points, params, south, north, west, east, ouput_dir, raster_dst):
+    start, end = f'{year}-01-01', f'{year}-12-31'
+    dt = pd.date_range(start, end, freq='D')
+    ds = mds.sel(date=slice(start, end))
+
+    results = np.ones((len(dt), len(params), points.shape[0])) * np.nan
+
+    for d, day in enumerate(ds.date.values):
         dt = pd.to_datetime(day)
         dt_str = dt.strftime("%Y%m%d")
-
         daily_data = ds.sel(date=day)
-        for parameter in params:
+
+        for i, parameter in enumerate(params):
+
+            if np.all(np.isnan(daily_data[parameter])):
+                continue
+
+            if np.count_nonzero(~np.isnan(daily_data[parameter].values)) == 1:
+                continue
 
             if raster_dst:
                 out_file = os.path.join(raster_dst, f'idw_{parameter}_{dt_str}.tif')
             else:
                 out_file = None
 
-            interpolated_values = interpolate_at_points(daily_data, parameter, points, resolution=0.05,
-                                                        lat=(s, n), lon=(w, e), out_raster=out_file)
-            results[parameter] = interpolated_values
+            try:
+                interp_vals = interpolate_at_points(daily_data, parameter, points.values, resolution=0.05,
+                                                    lat=(south, north), lon=(west, east), out_raster=out_file)
+            except Exception as exception_:
+                print(parameter, dt_str, exception_)
+                continue
 
-        results = pd.DataFrame().from_dict(results)
-        results.index = stations.index
-        data = pd.concat([stations, results], ignore_index=False, axis=1)
-        data.to_csv(output)
-        break
+            results[d, i, :] = interp_vals
+
+    for k, fid in enumerate(points.index):
+        df = pd.DataFrame(data=results[:, :, k], columns=params)
+        df['datetime'] = dt
+
+        out_dir = os.path.join(ouput_dir, fid)
+        if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+
+        out_file = os.path.join(out_dir, f'{fid}_{year}.csv')
+        df.to_csv(out_file, index=False)
 
 
 if __name__ == '__main__':
@@ -121,13 +159,17 @@ if __name__ == '__main__':
     aq_d = os.path.join(root, 'aq')
     aq_csv_data = os.path.join(root, 'aq', 'joined_data')
     aq_summary = os.path.join(aq_d, 'aqs.csv')
+
+    # aq_nc = os.path.join(aq_d, 'netCDF', 'aqs_mt.nc')
     aq_nc = os.path.join(aq_d, 'netCDF', 'aqs.nc')
-    aq_output = os.path.join(aq_d, 'dads_stations', 'aq_dads_test.csv')
+
+    aq_output = os.path.join(aq_d, 'dads_stations')
     aq_tif = os.path.join(aq_d, 'tif')
 
-    csv_to_netcdf(aq_csv_data, aq_summary, aq_nc)
+    # csv_to_netcdf(aq_csv_data, aq_summary, aq_nc)
 
+    workers = 10
     sites = os.path.join(root, 'met', 'stations', 'dads_stations_res_elev_mgrs.csv')
-    # interpolate_station_aq(aq_nc, aq_output, sites, raster_dst=None)
+    interpolate_station_aq(aq_nc, aq_output, sites, raster_dst=None)
 
 # ========================= EOF ====================================================================
