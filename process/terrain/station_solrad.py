e04ce5f6b3441bd5a3ea58da8b932a470e1917e1
dgketchum <dgketchum@gmail.com>
2024-10-29 12:48:03 -0600

    write solar DOY sequences station-wise

---
+++ process/terrain/station_solrad.py
@@ -0,0 +1,57 @@
+import os
+
+import pandas as pd
+
+
+def write_station_solrad(stations, tile_dir, station_out, shuffle=False, overwrite=False, index_col='fid'):
+    stations = pd.read_csv(stations)
+    stations.sort_index(inplace=True)
+
+    if shuffle:
+        stations = stations.sample(frac=1)
+
+    tiles = stations['MGRS_TILE'].unique().tolist()
+
+    for i, tile in enumerate(tiles, start=1):
+
+        if not isinstance(tile, str):
+            continue
+
+        f = os.path.join(tile_dir, f'tile_{tile}.csv')
+
+        if not os.path.exists(f):
+            print(f'{tile} data not found')
+            continue
+
+        df = pd.read_csv(f, index_col=0).T
+
+        for i, r in df.iterrows():
+
+            sf = os.path.join(station_out, f'{i}.csv')
+
+            if os.path.exists(sf) and not overwrite:
+                continue
+
+            r.to_csv(sf)
+
+        print(tile)
+
+
+if __name__ == '__main__':
+
+    d = '/media/research/IrrigationGIS/dads'
+    if not os.path.exists(d):
+        d = '/home/dgketchum/data/IrrigationGIS/dads'
+
+    out = os.path.join(d, 'dem', 'terrain', 'station_data')
+
+    # shapefile_path_ = os.path.join(root, 'met', 'stations', 'dads_stations_res_elev_mgrs_5071.shp')
+    # shapefile_path_ = os.path.join(root, 'climate', 'ghcn', 'stations', 'ghcn_CANUSA_stations_mgrs_5071.shp')
+    shapefile_path_ = os.path.join(d, 'met', 'stations', 'madis_mgrs_28OCT2024.csv')
+
+    solrad_tiled = os.path.join(d, 'dem', 'rsun_tables', 'madis_27OCT2024')
+    solrad_out = os.path.join(d, 'dem', 'rsun_tables', 'station_rsun')
+
+    write_station_solrad(shapefile_path_, solrad_tiled, solrad_out, shuffle=True, overwrite=False, index_col='fid')
+
+# ========================= EOF ====================================================================
