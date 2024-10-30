6a5e242779c5776394484b9be328ade1cde086e1
dgketchum <dgketchum@gmail.com>
2024-10-30 15:00:56 -0600

    multiproc file move

--- utils/move_samples.py
+++ utils/move_samples.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+import json
+import multiprocessing
 
 import geopandas as gpd
 
@@ -43,6 +45,63 @@ def organize_existing_samples(stations, csv_dir, output_dir):
         else:
             pass
 
+
+def copy_file(source_file, dest_file):
+    if os.path.exists(dest_file):
+        return
+    try:
+        shutil.copyfile(source_file, dest_file)
+        if dest_file.endswith('202312.csv'):
+            print(dest_file)
+    except Exception as e:
+        print(e, os.path.basename(source_file))
+
+
+def transfer_list(src, dst, workers=2):
+
+    sources = []
+    targets = []
+
+    dirs = os.listdir(src)
+    print(f'{len(dirs)} directories to copy')
+
+    for i, d in enumerate(dirs, start=1):
+
+        dst_dir = os.path.join(dst, d)
+        if not os.path.isdir(dst_dir):
+            os.mkdir(dst_dir)
+
+        src_dir = os.path.join(src, d)
+        src_filenames = [f for f in os.listdir(src_dir)]
+        dst_filenames = [f for f in os.listdir(dst_dir)]
+
+        sources.extend([os.path.join(src_dir, f) for f in src_filenames if f not in dst_filenames])
+        targets.extend([os.path.join(dst_dir, f) for f in src_filenames if f not in dst_filenames])
+
+        if i % 100 == 0:
+            print(f'\n{i}')
+            print(len(sources))
+            print(sources[-1])
+            print(len(targets))
+            print(targets[-1])
+
+        if len(sources) > 100000:
+
+            with multiprocessing.Pool(processes=workers) as pool:
+                pool.starmap(copy_file, zip(sources, targets))
+
+            sources = []
+            targets = []
+
 if __name__ == '__main__':
-    pass
+    d = '/media/research/IrrigationGIS'
+    if not os.path.exists(d):
+        d = '/home/dgketchum/data/IrrigationGIS'
+
+    mesonet_dir = os.path.join(d, 'climate', 'madis', 'LDAD', 'mesonet')
+    src_dir_ = os.path.join('/data/ssd1/madis', 'inclusive_csv')
+    dst_dir_ = os.path.join(mesonet_dir, 'inclusive_csv')
+
+    transfer_list(src_dir_, dst_dir_, workers=10)
+
 # ========================= EOF ====================================================================
