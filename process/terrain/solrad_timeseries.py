3b2b544752468c752163514493d8e5837a1eda27
dgketchum <dgketchum@gmail.com>
2024-11-01 11:08:55 -0600

    clean up solrad workflow

--- process/terrain/solrad_timeseries.py
+++ process/terrain/solrad_timeseries.py
@@ -8,7 +8,7 @@ from concurrent.futures import ProcessPoolExecutor
 
 
 def process_tile(tile, points, raster_dir, table_out, index_col='fid', overwrite=False):
-    write = False
+    """"""
     file_ = os.path.join(table_out, 'tile_{}.csv'.format(tile))
     if os.path.exists(file_) and not overwrite:
         return
@@ -17,41 +17,34 @@ def process_tile(tile, points, raster_dir, table_out, index_col='fid', overwrite
     tile_dir = os.path.join(raster_dir, tile)
     results = {k: {} for k, v in tile_points.iterrows()}
 
-    first, found = True, True
+    first, missing = True, False
     for day in range(1, 366):
         raster_file = os.path.join(tile_dir, f'irradiance_day_{day}_{tile}.tif')
 
         if not os.path.exists(raster_file):
-            if found:
-                print(f"Warning: Raster file not found for tile {tile}, day {day}. Skipping.")
-            found = False
-            write = False
-            continue
+            print(f"Warning: Raster file not found for tile {tile}, day {day}. Skipping.")
+            return
 
         values = zonal_stats(tile_points, raster_file, stats="mean", geojson_out=True)
 
         for v in values:
+
             if v['properties']['mean'] is None:
                 print('Empty value in {} for {}'.format(tile, v['properties'][index_col]))
-                continue
+                return
+
             if first:
                 results[v['id']] = {day: v['properties']['mean']}
+                first = False
             else:
                 results[v['id']][day] = v['properties']['mean']
 
-        first = False
-        if results[v['id']]:
-            write = True
-        else:
-            write = False
-
-    if write:
-        df = pd.DataFrame(results)
-        df.to_csv(file_)
-        print(os.path.basename(file_))
+    df = pd.DataFrame(results)
+    df.to_csv(file_)
+    print(os.path.basename(file_))
 
 
-def extract_raster_values_by_tile(shapefile_path, raster_dir, table_out, shuffle=False,
+def extract_raster_values_by_tile(shapefile_path, raster_dir, table_out, shuffle=False, avoid_tiles=None,
                                   overwrite=False, num_workers=2, index_col='fid'):
     points = gpd.read_file(shapefile_path)
     points.index = points[index_col]
@@ -60,6 +53,10 @@ def extract_raster_values_by_tile(shapefile_path, raster_dir, table_out, shuffle
 
     tiles = points['MGRS_TILE'].unique().tolist()
     print(f'{len(tiles)} tiles, {points.shape[0]} points')
+
+    if avoid_tiles:
+        tiles = [t for t in tiles if t not in avoid_tiles]
+        print(f'{len(tiles)} tiles after {len(avoid_tiles)} excluded')
 
     with ProcessPoolExecutor(max_workers=num_workers) as executor:
         executor.map(process_tile, tiles, [points] * len(tiles), [raster_dir] * len(tiles),
@@ -71,17 +68,17 @@ if __name__ == '__main__':
     if not os.path.exists(root):
         root = '/home/dgketchum/data/IrrigationGIS'
 
-    dem_d = os.path.join(root, 'dem')
-    mgrs = os.path.join(dem_d, 'w17_tiles.csv')
-
     # this must be EPSG:5071 shapefile
     # shapefile_path_ = os.path.join(root, 'met', 'stations', 'dads_stations_res_elev_mgrs_5071.shp')
     # shapefile_path_ = os.path.join(root, 'climate', 'ghcn', 'stations', 'ghcn_CANUSA_stations_mgrs_5071.shp')
     shapefile_path_ = os.path.join(root, 'dads', 'met', 'stations', 'madis_mgrs_28OCT2024_5071.shp')
 
+    avoid_tiles = os.path.join(root, 'boundaries', 'mgrs', 'mgrs_nldas_canada.csv')
+    avoid_tiles = pd.read_csv(avoid_tiles)['MGRS_TILE'].unique().tolist()
+
     raster_dir_ = os.path.join(root, 'dads', 'dem', 'rsun')
     solrad_out = os.path.join(root, 'dads', 'dem', 'rsun_tables', 'madis_28OCT2024')
-    extract_raster_values_by_tile(shapefile_path_, raster_dir_, solrad_out, num_workers=6,
+    extract_raster_values_by_tile(shapefile_path_, raster_dir_, solrad_out, num_workers=20, avoid_tiles=avoid_tiles,
                                   shuffle=True, overwrite=False, index_col='fid')
 
 # ========================= EOF ====================================================================
--- process/terrain/sun.py
+++ process/terrain/sun.py
@@ -6,17 +6,40 @@ from pprint import pprint
 import pandas as pd
 
 
-def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=False):
-    """remember to project DEM to EPSG:5071 and input the raster to the GRASS mapset with:
+def ingest_rasters(in_dir, tiles, mapset):
+    file_names = sorted(os.listdir(in_dir))
+    dem_files = [os.path.join(in_dir, f) for f in file_names if f.endswith('.tif')]
 
-    for file in /media/nvm/IrrigationGIS/dads/dem/proj/*.tif; do
-      r.in.gdal input="$file" output="$(basename "$file" .tif)"
+    for in_dem in dem_files:
+
+        tile = in_dem.split('.')[0][-5:]
+
+        if tile not in tiles:
+            continue
+
+        dem_name = f'dem_{tile}'
+        subprocess.call(['r.in.gdal', f'input={in_dem}', f'output={dem_name}@{mapset}', '--overwrite'])
+
+
+def calculate_terrain_irradiance(terrain_dir, terrain_source, mapset='PERMANENT', tiles=None, overwrite=False):
+    """
+    This must be run from a GRASS command line with the Location appropriate to the tile placement
+
+    For a new region, create a new location with an appropriate projection, e.g.,:
+    grass -c EPSG:3978 /media/nvm/IrrigationGIS/dads/dem/grassdata/canada
+    g.mapset -c mapset=dads_map_canada
+
+    The dem_{tile}.tif data also needs to be ingested into the mapset:
+
+    for file in /media/nvm/IrrigationGIS/dads/dem/dem_5071/*.tif; do
+        r.in.gdal input="$file" output="$(basename "$file" .tif)"
     done
+
 
     """
     slp = os.path.join(terrain_dir, 'slope')
     asp = os.path.join(terrain_dir, 'aspect')
-    d = os.path.join(terrain_dir, 'proj')
+    d = os.path.join(terrain_dir, terrain_source)
 
     dem_files = sorted(os.listdir(d))
     dem_names = sorted([f.split('.')[0] for f in dem_files if f.endswith('.tif')])
@@ -26,7 +49,11 @@ def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=Fals
     for dem_name, dem_file in zip(dem_names, dem_files):
 
         tile = dem_name.split('_')[-1]
-        print('\n', tile)
+        # print('\n', tile)
+
+        if tiles:
+            if tile not in tiles:
+                continue
 
         result = subprocess.run(['g.list', f'type=raster', f'pattern=irradiance_day_*_{tile}'],
                                 capture_output=True, text=True)
@@ -43,12 +70,12 @@ def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=Fals
         slope_output_path = os.path.join(slp, 'slope_{0}.tif'.format(tile))
         subprocess.call(['gdaldem', 'slope', f'{dem_file}', slope_output_path])
         slope_name = f'slope_{tile}'
-        subprocess.call(['r.in.gdal', f'input={slope_output_path}', f'output={slope_name}'])
+        subprocess.call(['r.in.gdal', f'input={slope_output_path}', f'output={slope_name}', '--overwrite'])
 
         aspect_output_path = os.path.join(asp, 'aspect_{0}.tif'.format(tile))
         subprocess.call(['gdaldem', 'aspect', f'{dem_file}', aspect_output_path])
         aspect_name = f'aspect_{tile}'
-        subprocess.call(['r.in.gdal', f'input={aspect_output_path}', f'output={aspect_name}'])
+        subprocess.call(['r.in.gdal', f'input={aspect_output_path}', f'output={aspect_name}', '--overwrite'])
 
         subprocess.call(['g.region', f'rast=dem_{tile}@{mapset}'])
 
@@ -67,16 +94,14 @@ def calculate_terrain_irradiance(terrain_dir, mapset='PERMANENT', overwrite=Fals
 
             dif = time.time() - start
             print('took {:.2f} seconds'.format(dif))
-        print(tile, '\n')
+        # print(tile, '\n')
 
 
 def export_rasters(terrain_dir, out_dir, mapset='PERMANENT', overwrite=False, mgrs_list=None):
-
-    d = os.path.join(terrain_dir, 'proj')
-
-    dem_files = sorted(os.listdir(d))
+    """"""
+    dem_files = sorted(os.listdir(terrain_dir))
     dem_names = sorted([f.split('.')[0] for f in dem_files if f.endswith('.tif')])
-    dem_files = [os.path.join(d, f) for f in dem_files]
+    dem_files = [os.path.join(terrain_dir, f) for f in dem_files if f.endswith('.tif')]
     print(f'{len(dem_files)} dem files to export')
 
     rsun_out = os.path.join(out_dir, 'rsun')
@@ -88,7 +113,7 @@ def export_rasters(terrain_dir, out_dir, mapset='PERMANENT', overwrite=False, mg
             print(f'{tile} exists but is not in list')
             continue
 
-        print('\n', tile)
+        # print('\n', tile)
 
         tile_dir = os.path.join(rsun_out, tile)
         if not os.path.isdir(tile_dir):
@@ -116,7 +141,25 @@ def export_rasters(terrain_dir, out_dir, mapset='PERMANENT', overwrite=False, mg
                  '--overwrite',
                  'output={0}'.format(irradiance_output_tif)])
 
-            print(tile, day)
+            # print(tile, day)
+
+
+def reproject_dems(in_dir, tiles, output_dir):
+    file_names = sorted(os.listdir(in_dir))
+    dem_files = [os.path.join(in_dir, f) for f in file_names if f.endswith('.tif')]
+    out_files = [os.path.join(output_dir, f) for f in file_names if f.endswith('.tif')]
+
+    for in_dem, out_dem in zip(dem_files, out_files):
+        tile = in_dem.split('.')[0][-5:]
+
+        if tile not in tiles:
+            continue
+        subprocess.run([
+            "gdalwarp", "-s_srs", "EPSG:5071", '-overwrite',
+            "-t_srs", "EPSG:3978", "-r", "bilinear",
+            "-of", "GTiff", in_dem, out_dem
+        ])
+        print(tile, os.path.basename(in_dem), os.path.basename(out_dem))
 
 
 def remove_rasters(terrain_dir, resolution=300):
@@ -150,14 +193,21 @@ if __name__ == '__main__':
     dem_d = os.path.join(root, 'dads', 'dem')
     out_dem = os.path.join(out, 'dads', 'dem')
 
-    calculate_terrain_irradiance(dem_d, mapset="dads_map", overwrite=False)
+    tiles_ = os.path.join(out, 'boundaries', 'mgrs', 'mgrs_nldas_canada.csv')
+    tiles_ = pd.read_csv(tiles_)['MGRS_TILE'].unique().tolist()
+
+    conus_dir_ = os.path.join(dem_d, 'dem_5071')
+    canada_dir_ = os.path.join(dem_d, 'dem_3978')
+    # reproject_dems(conus_dir_, tiles_, canada_dir_)
+
+    ingest_rasters(canada_dir_, tiles_, mapset="dads_map_canada")
+
+    calculate_terrain_irradiance(dem_d, canada_dir_, mapset="dads_map_canada", tiles=tiles_, overwrite=True)
 
     # stations_out = os.path.join(out, 'met', 'stations', 'dads_stations_res_elev_mgrs.csv')
-    tiles = os.path.join(out, 'boundaries', 'mgrs', 'mgrs_wgs.csv')
     # stations_out = os.path.join(out, 'met', 'stations', 'madis_mgrs_28OCT2024.csv')
 
-    tiles = pd.read_csv(tiles)['MGRS_TILE'].unique().tolist()
-    export_rasters(dem_d, out_dem, mapset="dads_map", overwrite=False, mgrs_list=tiles)
+    export_rasters(canada_dir_, out_dem, mapset="dads_map_canada", mgrs_list=tiles_, overwrite=True)
 
     # remove_rasters(dem_d, resolution=30)
 
