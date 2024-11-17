00aae750a508a2b875a54ab85ec9c986f71c4333
dgketchum <dgketchum@gmail.com>
2024-11-17 12:35:38 -0700

    daymet extract success

--- extract/met_data/gridded_nc/daymet_extract.py
+++ extract/met_data/gridded_nc/daymet_extract.py
@@ -5,6 +5,8 @@ import earthaccess
 import numpy as np
 import pandas as pd
 import xarray as xr
+import pyproj
+import cartopy.crs as ccrs
 
 
 def get_daymet(start_date, end_date, down_dst=None):
@@ -28,18 +30,13 @@ def extract_daymet(stations, out_data, nc_data=None, workers=8, overwrite=False,
         w, s, e, n = bounds
         station_list = station_list[(station_list['latitude'] < n) & (station_list['latitude'] >= s)]
         station_list = station_list[(station_list['longitude'] < e) & (station_list['longitude'] >= w)]
-    else:
-        ln = station_list.shape[0]
-        w, s, e, n = (-125.0, 25.0, -67.0, 53.0)
-        station_list = station_list[(station_list['latitude'] < n) & (station_list['latitude'] >= s)]
-        station_list = station_list[(station_list['longitude'] < e) & (station_list['longitude'] >= w)]
-        print('dropped {} stations outside DAYMET extent'.format(ln - station_list.shape[0]))
 
     print(f'{len(station_list)} stations to write')
 
     station_list = station_list.sample(frac=1)
     station_list = station_list.rename(columns={'latitude': 'lat', 'longitude': 'lon'})
-    indexer = station_list[['lat', 'lon']].to_xarray()
+    station_list[['x', 'y']] = station_list.apply(projected_coords, axis=1, result_type='expand')
+    indexer = station_list[['x', 'y']].to_xarray()
     fids = np.unique(indexer.fid.values).tolist()
 
     years, files = [], []
@@ -48,11 +45,13 @@ def extract_daymet(stations, out_data, nc_data=None, workers=8, overwrite=False,
         nc_files = []
         granules = get_daymet(f'{year}-01-01', f'{year}-01-31')
         for granule in granules:
-            split = granule['meta']['native-id'].split('_')
+            nc_id = granule['meta']['native-id']
+            split = nc_id.split('_')
             region, param = split[5], split[6]
-            # 'tmin', 'vp', 'prcp', 'srad'
-            if param in ['tmax'] and region == 'na':
-                nc_files.append(granule)
+            file_name = '.'.join(nc_id.split('.')[1:])
+            if param in ['tmax', 'tmin', 'vp', 'prcp', 'srad'] and region == 'pr':
+                target_file = os.path.join(nc_dir, file_name)
+                nc_files.append((target_file, granule))
 
         if not nc_files:
             print(f"No NetCDF files found for {year}")
@@ -73,16 +72,19 @@ def extract_daymet(stations, out_data, nc_data=None, workers=8, overwrite=False,
         concurrent.futures.wait(futures)
 
 
-def proc_time_slice(granules_, indexer_, date_string_, fids_, out_, overwrite_, par_check=None, nc_dir_=None):
+def proc_time_slice(fileset_, indexer_, date_string_, fids_, out_, overwrite_, par_check=None, nc_dir_=None):
+    local_files, granules = [f[0] for f in fileset_], [f[1] for f in fileset_]
     try:
-        ges_files = earthaccess.download(granules_, nc_dir_, threads=4)
-        ds = xr.open_mfdataset(ges_files, engine='netcdf4')
+        if not all([os.path.exists(f) for f in local_files]):
+            local_files = earthaccess.download(granules, nc_dir_, threads=4)
+        ds = xr.open_mfdataset(local_files, engine='netcdf4')
     except Exception as exc:
+        print(f'{exc}')
         return
 
-    ds = ds.sel(lat=indexer_.lat, lon=indexer_.lon, method='nearest')
-    time_values = pd.to_datetime(ds['time'].values, unit='h', origin=pd.Timestamp('1979-01-01'))
-    ds = ds.assign_coords(time=time_values).set_index(time='time')
+    ds = ds.chunk({'time': len(ds.time.values)})
+    ds = ds.sel(y=indexer_.y, x=indexer_.x, method='nearest')
+    df_all = ds.to_dataframe()
     ct = 0
     for fid in fids_:
         dst_dir = os.path.join(out_, fid)
@@ -91,13 +93,26 @@ def proc_time_slice(granules_, indexer_, date_string_, fids_, out_, overwrite_, 
         _file = os.path.join(dst_dir, '{}_{}.csv'.format(fid, date_string_))
 
         if not os.path.exists(_file) or overwrite_:
-            df_station = ds.sel(fid=fid).to_dataframe()
-            df_station = df_station.groupby(df_station.index.get_level_values('time')).first()
-            df_station['dt'] = [i.strftime('%Y%m%d%H') for i in df_station.index]
+            df_station = df_all.loc[(fid, slice(None), 0)].copy()
+            df_station['dt'] = [i.strftime('%Y%m%d') for i in df_station.index]
             df_station.to_csv(_file, index=False)
             ct += 1
     print(f'wrote {ct} for {date_string_}')
 
+
+def projected_coords(row):
+    globe = ccrs.Globe(ellipse='sphere', semimajor_axis=6370000, semiminor_axis=6370000)
+    lcc = ccrs.LambertConformal(globe=globe,
+                                central_longitude=-100,
+                                central_latitude=42.5,
+                                standard_parallels=(25, 60))
+
+    lcc_wkt = lcc.to_wkt()
+    source_crs = 'epsg:4326'
+    transformer = pyproj.Transformer.from_crs(source_crs, lcc_wkt)
+
+    x, y = transformer.transform(row['lat'], row['lon'])
+    return x, y
 
 if __name__ == '__main__':
 
@@ -116,7 +131,8 @@ if __name__ == '__main__':
 
     out_files = os.path.join(daymet, 'station_data')
     nc_files = os.path.join(daymet, 'netcdf')
+    bounds = (-68.0, 17.0, -64.0, 20.0)
 
-    extract_daymet(sites, out_files, workers=14, overwrite=False, bounds=None, debug=True, nc_dir=nc_files)
+    extract_daymet(sites, out_files, workers=14, overwrite=False, bounds=bounds, debug=True, nc_dir=nc_files)
 
 # ========================= EOF ====================================================================
