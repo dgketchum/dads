eb6e750d7270700b3034351191018dcfa0e17d01
dgketchum <dgketchum@gmail.com>
2024-11-29 20:13:03 -0700

    completed nldas2 extract

--- extract/met_data/grid/era5_extract.py
+++ extract/met_data/grid/era5_extract.py
@@ -1,13 +1,55 @@
+import calendar
 import os
-import os
+
+import cdsapi
 import pandas as pd
 import xarray as xr
 
 from utils.station_parameters import station_par_map
 
+
+def download_era5(target_dir):
+    dataset = "reanalysis-era5-land"
+    request = {
+        "variable": [
+            "2m_dewpoint_temperature",
+            "2m_temperature",
+            "surface_solar_radiation_downwards",
+            "10m_u_component_of_wind",
+            "10m_v_component_of_wind",
+            "surface_pressure",
+            "total_precipitation"
+        ],
+        "time": [
+            "00:00", "01:00", "02:00",
+            "03:00", "04:00", "05:00",
+            "06:00", "07:00", "08:00",
+            "09:00", "10:00", "11:00",
+            "12:00", "13:00", "14:00",
+            "15:00", "16:00", "17:00",
+            "18:00", "19:00", "20:00",
+            "21:00", "22:00", "23:00"
+        ],
+        "data_format": "netcdf",
+        "download_format": "unarchived",
+        "area": [53, -125, 25, -67]
+    }
+
+    client = cdsapi.Client()
+
+    for year in range(1990, 20244):
+        for month in range(1, 13):
+            _, num_days = calendar.monthrange(year, month)
+            request["day"] = [str(day) for day in range(1, num_days + 1)]
+            request["year"] = str(year)
+            request["month"] = f"{month:02d}"
+            target_nc = os.path.join(target_dir, f"era5_land_{year}_{month:02d}.nc")
+            client.retrieve(dataset, request, target_nc)
+            print(f'downloaded {target_nc}')
+
+
 def extract_met_data(stations, gridded_dir, nc_file, overwrite=False,
                      station_type='dads', shuffle=True, bounds=None):
-
     kw = station_par_map(station_type)
 
     station_list = pd.read_csv(stations, index_col='index')
@@ -72,5 +114,18 @@ def extract_met_data(stations, gridded_dir, nc_file, overwrite=False,
 
 
 if __name__ == '__main__':
-    pass
+
+    d = os.path.join('/home', 'dgketchum', 'data', 'IrrigationGIS')
+
+    if not os.path.isdir(d):
+        d = os.path.join('/data', 'IrrigationGIS')
+        temp_directory = os.path.join('/data', 'temp')
+
+    if not os.path.isdir(d):
+        d = os.path.join('/home', 'dketchum', 'data', 'IrrigationGIS')
+
+    nc_dir = os.path.join(d)
+
+    download_era5(nc_dir)
+
 # ========================= EOF ====================================================================
