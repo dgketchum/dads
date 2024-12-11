ee6f57c4a95e98ff25f0ac04a5cbe26480fbe00c
dgketchum <dgketchum@gmail.com>
2024-12-11 16:49:13 -0700

    trying to get rtma

--- extract/met_data/grid/rtma_extact.py
+++ extract/met_data/grid/rtma_extact.py
@@ -2,10 +2,13 @@ import calendar
 import concurrent.futures
 import gc
 import os
+
+import json
 from datetime import datetime
 
 import numpy as np
 import pandas as pd
+import pyproj
 import xarray as xr
 from herbie import FastHerbie
 import boto3
@@ -13,29 +16,35 @@ from botocore.exceptions import ClientError
 from botocore import UNSIGNED
 from botocore.client import Config
 import cfgrib
+import xoak
 import pygrib
 from pyproj import CRS
+import numpy as np
+from sklearn.neighbors import BallTree
+from sklearn.metrics.pairwise import haversine_distances
+
+"""
+NOAA GRIB Projection Info: https://www.nco.ncep.noaa.gov/pmb/docs/on388/tableb.html
+"""
 
 
 def extract_rtma(stations, out_data, grib, netcdf, model="rtma", workers=8, overwrite=False, bounds=None,
-                 debug=False, stream=False, start_yr=1990, end_yr=2024):
+                 debug=False, start_yr=1990, end_yr=2024):
     """"""
-    # TODO: remove the nrows arg!!!
-    station_list = pd.read_csv(stations, nrows=100)
+    station_list = pd.read_csv(stations, nrows=15)
     if 'LAT' in station_list.columns:
         station_list = station_list.rename(columns={'STAID': 'fid', 'LAT': 'latitude', 'LON': 'longitude'})
     station_list.index = station_list['fid']
 
-    if bounds:
-        w, s, e, n = bounds
-        station_list = station_list[(station_list['latitude'] < n) & (station_list['latitude'] >= s)]
-        station_list = station_list[(station_list['longitude'] < e) & (station_list['longitude'] >= w)]
-    else:
-        ln = station_list.shape[0]
-        w, s, e, n = (-125.0, 25.0, -67.0, 53.0)
-        station_list = station_list[(station_list['latitude'] < n) & (station_list['latitude'] >= s)]
-        station_list = station_list[(station_list['longitude'] < e) & (station_list['longitude'] >= w)]
-        print('dropped {} stations outside RTMA extent'.format(ln - station_list.shape[0]))
+    w, s, e, n = bounds
+    print(f'Geo Bounds Extent: x ({w}, {e}), y ({s}, {n})')
+
+    station_list = station_list[(station_list['latitude'] < n) & (station_list['latitude'] >= s)]
+    station_list = station_list[(station_list['longitude'] < e) & (station_list['longitude'] >= w)]
+
+    if len(station_list) < 1:
+        print('No stations found in this area')
+        return
 
     print(f'{len(station_list)} stations to write')
 
@@ -46,8 +55,8 @@ def extract_rtma(stations, out_data, grib, netcdf, model="rtma", workers=8, over
         station_list['end_dt'] = [pd.to_datetime(r['END']) for i, r in station_list.iterrows()]
         station_list = station_list[station_list['end_dt'] > pd.to_datetime('2016-01-01')]
 
-    indexer = station_list[['lat', 'lon']].to_xarray()
-    fids = np.unique(indexer.fid.values).tolist()
+    station_list = station_list.rename(columns={'lat': 'slat', 'lon': 'slon'})
+    indexer = station_list[['slat', 'slon']].to_xarray()
 
     yrmo = [f'{year}{month:02}' for year in range(start_yr, end_yr + 1) for month in range(1, 13)]
     yrmo = [yms for yms in yrmo if int(yms[:4]) > 2014]
@@ -56,37 +65,45 @@ def extract_rtma(stations, out_data, grib, netcdf, model="rtma", workers=8, over
 
     if debug:
         for dts in yrmo:
-            proc_time_slice(indexer, dts, fids, grib, netcdf, out_data, overwrite, model, stream)
+            if dts != '201706':
+                continue
+            proc_time_slice(indexer, dts, grib, netcdf, out_data, overwrite, model)
 
     with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
-        futures = [executor.submit(proc_time_slice, indexer, dts, fids, grib, netcdf, out_data, overwrite, model, stream)
+        futures = [executor.submit(proc_time_slice, indexer, dts, grib, netcdf, out_data, overwrite, model)
                    for dts in yrmo]
         concurrent.futures.wait(futures)
 
 
-def proc_time_slice(indexer_, date_string_, fids_, grb_, nc_, out_, overwrite_, model_, stream_=False):
+def proc_time_slice(indexer_, date_string_, grb_, nc_, out_, overwrite_, model_):
     """"""
     nc_file = os.path.join(nc_, f'{model}_{date_string_}.nc')
 
     if not os.path.exists(nc_file):
-        ds = get_grb_files(date_string_, model_, None, stream_data=stream_)
+        ds = get_grb_files(date_string_, model_, grb_, nc_file)
     else:
         ds = xr.open_dataset(nc_file)
-    return
 
-    ds = ds.chunk({'time': ds.time.values.shape[0], 'y': 40, 'x': 60})
-    ds.to_netcdf(nc_file)
+    # was unable to use xoak here for some reason it dropped FID index
+    # must use a tree aproach as the only meaningful spatial coordinates are in an irregular grid
+    grid_points_radians = np.deg2rad(np.stack((ds['latitude'].values.ravel(), ds['longitude'].values.ravel()), axis=-1))
+    station_points_radians = np.deg2rad(np.stack((indexer_['slat'].values, indexer_['slon'].values), axis=-1))
+    tree = BallTree(grid_points_radians, metric='haversine')
+    distances, indices = tree.query(station_points_radians, k=1)
+    earth_radius = 6371
+    distances_km = distances * earth_radius
+    indexer_['distance'] = xr.DataArray(distances_km.flatten(), dims='fid')
+    indices = indices.reshape(-1)
+    y_indices = indices // ds.dims['x']
+    x_indices = indices % ds.dims['x']
+    ds = ds.sel(x=x_indices, y=y_indices)
+    ds = xr.merge([ds, indexer_],  compat='override')
+    df_all = ds.to_dataframe()
+    ct, skip = 0, 0
+    now = datetime.strftime(datetime.now(), '%Y%m%d %H:%M')
+    print(f'prepare to write {date_string_}: {now}')
 
-    try:
-        ds = ds.sel(latitude=indexer_.lat, longitude=indexer_.lon, method='nearest')
-        df_all = ds.to_dataframe()
-        ct, skip = 0, 0
-        now = datetime.strftime(datetime.now(), '%Y%m%d %H:%M')
-        print(f'prepare to write {date_string_}: {now}')
-    except Exception as exc:
-        print(f'{exc} on {date_string_}')
-        return
-
+    fids_ = np.unique(indexer_.fid.values).tolist()
     for fid in fids_:
 
         try:
@@ -100,7 +117,7 @@ def proc_time_slice(indexer_, date_string_, fids_, grb_, nc_, out_, overwrite_, 
                 os.remove(_file)
 
             if not os.path.exists(_file) or overwrite_:
-                df_station = df_all.loc[(slice(None), fid)].copy()
+                df_station = df_all.loc[(0, 0, slice(None), fid)].copy()
                 df_station['dt'] = [i.strftime('%Y%m%d%H') for i in df_station.index]
                 df_station.to_csv(_file, index=False)
                 ct += 1
@@ -118,14 +135,14 @@ def proc_time_slice(indexer_, date_string_, fids_, grb_, nc_, out_, overwrite_, 
     print(f'wrote {ct} for {date_string_}, skipped {skip}, {now}')
 
 
-def get_grb_files(date_str, model, dst=None, max_threads=6, stream_data=False):
+def get_grb_files(date_str, model, dst, nc_file, max_threads=6):
     """"""
 
     s3 = boto3.client('s3', config=Config(signature_version=UNSIGNED))
 
     year, month = int(date_str[:4]), int(date_str[-2:])
     month_end = calendar.monthrange(year, month)[1]
-    start, end = f'{year}-{month}-01 00:00', f'{year}-{month:02}-{month_end:02} 23:00'
+    start, end = f'{year}-{month}-01 00:00', f'{year}-{month:02}-{1:02} 06:00'
 
     dates = pd.date_range(start=start, end=end, freq='1h')
     FH = FastHerbie(dates, model=model, max_threads=max_threads, **{'save_dir': dst})
@@ -135,24 +152,23 @@ def get_grb_files(date_str, model, dst=None, max_threads=6, stream_data=False):
         if not os.path.exists(target_dir):
             os.mkdir(target_dir)
 
-    if not stream_data and dst:
-        for obj in FH.objects:
-            try:
-                remote_file = '/'.join(obj.SOURCES['aws'].split('/')[-2:])
-                local_file = os.path.basename(obj.SOURCES['aws'])
-                local_path = os.path.join(target_dir, local_file)
-                obj.SOURCES['local'] = local_path
+    for obj in FH.objects:
+        try:
+            remote_file = '/'.join(obj.SOURCES['aws'].split('/')[-2:])
+            local_file = os.path.basename(obj.SOURCES['aws'])
+            local_path = os.path.join(target_dir, local_file)
+            obj.SOURCES['local'] = local_path
 
-                if not os.path.exists(local_path):
-                    s3.download_file(f'noaa-{model}-pds', remote_file, local_path)
-                    print(f'downloaded {local_path}')
+            if not os.path.exists(local_path):
+                s3.download_file(f'noaa-{model}-pds', remote_file, local_path)
+                print(f'downloaded {local_path}')
 
-            except Exception as exc:
-                print(exc, date_str, 'on download')
-                return None
+        except Exception as exc:
+            print(exc, date_str, 'on download')
+            return None
 
     try:
-        ds_list = [to_xarray(H) for H in FH.objects]
+        ds_list = [to_xarray(obj, model) for obj in FH.objects]
         ds_list = [xr.merge(dso, compat='override') for dso in ds_list]
         ds_list.sort(key=lambda x: x.time.data.max())
     except Exception as exc:
@@ -162,10 +178,15 @@ def get_grb_files(date_str, model, dst=None, max_threads=6, stream_data=False):
     ds_ = xr.combine_nested(ds_list, combine_attrs='drop_conflicts', concat_dim='time')
     ds_ = ds_.squeeze()
 
+    ds_['longitude'] = ds_['longitude'] - 360.
+
+    ds_ = ds_.chunk({'time': ds_.time.values.shape[0], 'y': 40, 'x': 60})
+    ds_.to_netcdf(nc_file)
+
     return ds_
 
 
-def to_xarray(h_object, model):
+def to_xarray(h_object, model_):
     backend_kwargs = {}
     backend_kwargs.setdefault('indexpath', '')
     backend_kwargs.setdefault(
@@ -174,34 +195,28 @@ def to_xarray(h_object, model):
     )
     backend_kwargs.setdefault('errors', 'raise')
 
+    Hxr = cfgrib.open_datasets(h_object.SOURCES['local'], backend_kwargs=backend_kwargs)
 
-    Hxr = cfgrib.open_datasets(h_object, backend_kwargs=backend_kwargs)
-
-    with pygrib.open(str(local_file)) as grb:
-        msg = grb.message(1)
-        cf_params = CRS(msg.projparams).to_cf()
+    with open('rtma_cf_params.json', 'r') as f:
+        cf_params = json.load(f)
 
     for ds in Hxr:
-        ds.attrs['model'] = str(self.model)
-        ds.attrs['product'] = str(self.product)
-        ds.attrs['description'] = self.DESCRIPTION
-        ds.attrs['remote_grib'] = str(self.grib)
-        ds.attrs['local_grib'] = str(local_file)
-        ds.attrs['search'] = str(search)
+        ds.attrs['model'] = str(model_)
+        ds.attrs['product'] = 'surface_meteorology'
+        ds.attrs['description'] = f'{model_} surface meteorology'
+        ds.attrs['remote_grib'] = str(h_object.SOURCES['aws'])
+        ds.attrs['local_grib'] = str(h_object.SOURCES['local'])
+        ds.attrs['search'] = 'all'
         ds.coords['gribfile_projection'] = None
         ds.coords['gribfile_projection'].attrs = cf_params
         ds.coords['gribfile_projection'].attrs['long_name'] = (
-            f'{self.model.upper()} model grid projection'
+            f'{model_.upper()} model grid projection'
         )
-
-        # Assign this grid_mapping for all variables
         for var in list(ds):
             ds[var].attrs['grid_mapping'] = 'gribfile_projection'
 
-        if len(Hxr) == 1:
-            return Hxr[0]
-        else:
-            raise ValueError
+    return Hxr
+
 
 def get_quadrants(b):
     mid_longitude = (b[0] + b[2]) / 2
@@ -237,13 +252,13 @@ if __name__ == '__main__':
     out_netcdf_ = os.path.join(rtma, 'netcdf')
     out_grib_ = os.path.join(rtma, 'grib')
 
-    bounds_ = (-124.0, 23.0, -66.0, 52.0)
+    bounds_ = (-124.0, 19.2, -66.0, 53.0)
     quadrants = get_quadrants(bounds_)
 
     for e, quad in enumerate(quadrants, start=1):
         print(f'\n\n\n\n Quadrant {e} \n\n\n\n')
 
         extract_rtma(sites, out_files, out_grib_, out_netcdf_, model='rtma', workers=1, overwrite=False,
-                     bounds=quad, debug=True, stream=True, start_yr=2020, end_yr=2024)
+                     bounds=quad, debug=True, start_yr=2017, end_yr=2024)
 
 # ========================= EOF ====================================================================
