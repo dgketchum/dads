3e802e8f413f1c9fdff1dd4aec83cae8d2708862
dgketchum <dgketchum@gmail.com>
2025-02-07 15:01:21 -0700

    update gridded sources processing

--- process/gridded/proc_gridded.py
+++ process/gridded/proc_gridded.py
@@ -4,6 +4,8 @@ import pytz
 import numpy as np
 import pandas as pd
 from pandarallel import pandarallel
+import pyarrow
+
 from refet import calcs
 
 from utils.station_parameters import station_par_map
@@ -23,7 +25,7 @@ NLDAS_RESAMPLE_MAP = {'rsds': 'sum',
 
 
 def extract_met_data(stations, gridded_dir, overwrite=False, station_type='openet',
-                     shuffle=True, bounds=None, hourly=False, **targets):
+                     shuffle=True, bounds=None, hourly=False, pd_parallel=False, **targets):
     """"""
     kw = station_par_map(station_type)
 
@@ -34,12 +36,17 @@ def extract_met_data(stations, gridded_dir, overwrite=False, station_type='opene
 
     if bounds:
         w, s, e, n = bounds
-        station_list = station_list[(station_list['latitude'] < n) & (station_list['latitude'] >= s)]
-        station_list = station_list[(station_list['longitude'] < e) & (station_list['longitude'] >= w)]
+        station_list = station_list[(station_list[kw['lat']] < n) & (station_list[kw['lat']] >= s)]
+        station_list = station_list[(station_list[kw['lon']] < e) & (station_list[kw['lon']] >= w)]
+
+    out_file_ = None
+    out_file_gm = None
+    out_file_prism = None
+    out_file_dm = None
 
     for i, (fid, row) in enumerate(station_list.iterrows(), start=1):
 
-        # if fid != 'COVM':
+        # if fid != 'USC00265705':
         #     continue
 
         lon, lat, elv = row[kw['lon']], row[kw['lat']], row[kw['elev']]
@@ -49,71 +56,96 @@ def extract_met_data(stations, gridded_dir, overwrite=False, station_type='opene
 
         if targets['nldas2']:
             in_file_ = os.path.join(gridded_dir, 'raw_parquet', 'nldas2', '{}.parquet.gzip'.format(fid))
+
             if not os.path.exists(in_file_):
-                print('Input NLDAS at {} does not exist, skipping'.format(os.path.basename(in_file_)))
-                continue
+                print(f'NLDAS at {os.path.basename(in_file_)} does not exist, skipping, {lat:.1f} {lon:.1f}')
 
-            if hourly:
-                out_file_ = os.path.join(gridded_dir, 'processed_parquet', 'nldas2_hourly', '{}.parquet'.format(fid))
             else:
-                out_file_ = os.path.join(gridded_dir, 'processed_parquet', 'nldas2', '{}.parquet'.format(fid))
+                if hourly:
+                    out_file_ = os.path.join(gridded_dir, 'processed_parquet', 'nldas2_hourly',
+                                             '{}.parquet'.format(fid))
+                else:
+                    out_file_ = os.path.join(gridded_dir, 'processed_parquet', 'nldas2', '{}.parquet'.format(fid))
 
-            if not os.path.exists(out_file_) or overwrite:
-                proc_nldas(in_file_=in_file_, lat=lat, elev=elv, out_file_=out_file_, hourly_=hourly)
-                print('nldas', fid)
-            else:
-                print('nldas {} exists'.format(fid))
-                pass
+                if not os.path.exists(out_file_) or overwrite:
+                    proc_nldas(in_file_=in_file_, lat=lat, elev=elv, out_file_=out_file_,
+                               hourly_=hourly, parallel=pd_parallel)
+                    print('nldas', fid)
+                else:
+                    print('nldas {} exists'.format(fid))
+                    pass
 
         if targets['gridmet']:
 
             in_file_ = os.path.join(gridded_dir, 'raw_parquet', 'gridmet', '{}.parquet.gzip'.format(fid))
             if not os.path.exists(in_file_):
-                print('Input GridMET at {} does not exist, skipping'.format(os.path.basename(in_file_)))
-                continue
+                print(f'GridMET at {os.path.basename(in_file_)} does not exist, skipping, {lat:.1f} {lon:.1f}')
 
-            out_file_gm = os.path.join(gridded_dir, 'processed_parquet', 'gridmet', '{}.parquet'.format(fid))
-
-            if not os.path.exists(out_file_gm) or overwrite:
-                proc_gridmet(in_file_=in_file_, lat=lat, elev=elv, out_file_=out_file_gm)
-                print('gridmet', fid)
             else:
-                pass
+                out_file_gm = os.path.join(gridded_dir, 'processed_parquet', 'gridmet', '{}.parquet'.format(fid))
+
+                if not os.path.exists(out_file_gm) or overwrite:
+                    proc_gridmet(in_file_=in_file_, lat=lat, elev=elv, out_file_=out_file_gm, parallel=pd_parallel)
+                    print('gridmet', fid)
+                else:
+                    pass
 
         if targets['prism']:
 
             in_file_ = os.path.join(gridded_dir, 'raw_parquet', 'prism', '{}.parquet.gzip'.format(fid))
             if not os.path.exists(in_file_):
-                print('Input PRISM at {} does not exist, skipping'.format(os.path.basename(in_file_)))
-                continue
+                print(f'PRISM at {os.path.basename(in_file_)} does not exist, skipping, {lat:.1f} {lon:.1f}')
 
-            out_file_gm = os.path.join(gridded_dir, 'processed_parquet', 'prism', '{}.parquet'.format(fid))
-
-            if not os.path.exists(out_file_gm) or overwrite:
-                proc_prism(in_file_=in_file_, elev=elv, out_file_=out_file_gm)
-                print('prism', fid)
             else:
-                pass
+                out_file_prism = os.path.join(gridded_dir, 'processed_parquet', 'prism', '{}.parquet'.format(fid))
+
+                if not os.path.exists(out_file_prism) or overwrite:
+                    proc_prism(in_file_=in_file_, elev=elv, out_file_=out_file_prism)
+                    print('prism', fid)
+                else:
+                    pass
 
         if targets['daymet']:
 
             in_file_ = os.path.join(gridded_dir, 'raw_parquet', 'daymet', '{}.parquet.gzip'.format(fid))
             if not os.path.exists(in_file_):
-                print('Input DAYMET at {} does not exist, skipping'.format(os.path.basename(in_file_)))
-                continue
+                print(f'DAYMET at {os.path.basename(in_file_)} does not exist, skipping, {lat:.1f} {lon:.1f}')
 
-            out_file_gm = os.path.join(gridded_dir, 'processed_parquet', 'daymet', '{}.parquet'.format(fid))
-
-            if not os.path.exists(out_file_gm) or overwrite:
-                proc_daymet(in_file_=in_file_, lat=lat, elev=elv, out_file_=out_file_gm)
-                print('daymet', fid)
             else:
+                out_file_dm = os.path.join(gridded_dir, 'processed_parquet', 'daymet', '{}.parquet'.format(fid))
+
+                if not os.path.exists(out_file_dm) or overwrite:
+                    proc_daymet(in_file_=in_file_, elev=elv, out_file_=out_file_dm)
+                    print('daymet', fid)
+                else:
+                    pass
+
+        if fid == 'USC00265705':
+            target_files = {'nldas2': out_file_, 'gridmet': out_file_gm,
+                            'prism': out_file_prism, 'daymet': out_file_dm}
+
+            compare_sources(**target_files)
+
+
+def compare_sources(**targets):
+    dfs = {}
+    for k, v in targets.items():
+        dfs[k] = pd.read_parquet(v).loc['1991-01-01': '2023-12-31']
+
+    params = ['prcp', 'vpd', 'tmean', 'u2', 'eto']
+
+    for p in params:
+        for k, v in dfs.items():
+            if k in ['prism', 'daymet'] and p in ['u2', 'eto']:
                 pass
+            else:
+                print(f'{p} - {k} mean: {v[p].mean()}')
 
-def proc_nldas(in_file_, lat, elev, out_file_, hourly_=False):
-    df = pd.read_parquet(in_file_)
 
+def proc_nldas(in_file_, lat, elev, out_file_, hourly_=False, parallel=False):
     try:
+        df = pd.read_parquet(in_file_)
+
         wind_u = df['Wind_E']
         wind_v = df['Wind_N']
         df['wind'] = np.sqrt(wind_v ** 2 + wind_u ** 2)
@@ -144,26 +176,31 @@ def proc_nldas(in_file_, lat, elev, out_file_, hourly_=False):
 
         df['doy'] = [i.dayofyear for i in df.index]
 
-        asce_params = df.parallel_apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
-        # asce_params = df.apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
+        if parallel:
+            asce_params = df.parallel_apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
+        else:
+            asce_params = df.apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
+
         df[['tmean', 'vpd', 'rn', 'u2', 'eto']] = pd.DataFrame(asce_params.tolist(),
                                                                index=df.index)
+
         df['year'] = [i.year for i in df.index]
         df['date_str'] = [i.strftime('%Y-%m-%d') for i in df.index]
 
-    except KeyError as exc:
+    except (KeyError, pyarrow.lib.ArrowInvalid, FileNotFoundError) as exc:
         bad_files = os.path.join(os.path.dirname(__file__), 'bad_files.txt')
         with open(bad_files, 'a') as f:
-            f.write(in_file_ + '\n')
+            f.write(in_file_ + ' ' + ' '.join(exc.args) + '\n')
+        print(os.path.basename(in_file_), ' '.join(exc.args))
         return None
 
     df.to_parquet(out_file_)
 
 
-def proc_gridmet(in_file_, lat, elev, out_file_):
-    df = pd.read_parquet(in_file_)
-
+def proc_gridmet(in_file_, lat, elev, out_file_, parallel=False):
     try:
+        df = pd.read_parquet(in_file_)
+
         df['tmin'] = df['tmmn'] - 273.15
         df['tmax'] = df['tmmx'] - 273.15
         df['q'] = df['sph']
@@ -180,55 +217,56 @@ def proc_gridmet(in_file_, lat, elev, out_file_):
 
         df['prcp'] = df['pr']
 
-        asce_params = df.parallel_apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
-        # asce_params = df.apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
+        if parallel:
+            asce_params = df.parallel_apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
+        else:
+            asce_params = df.apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
+
         df[['tmean', 'vpd', 'rn', 'u2', 'eto']] = pd.DataFrame(asce_params.tolist(),
                                                                index=df.index)
 
-    except KeyError as exc:
+    except (KeyError, pyarrow.lib.ArrowInvalid, FileNotFoundError) as exc:
         bad_files = os.path.join(os.path.dirname(__file__), 'bad_files.txt')
         with open(bad_files, 'a') as f:
-            f.write(in_file_ + '\n')
+            f.write(in_file_ + ' ' + ' '.join(exc.args) + '\n')
+        print(os.path.basename(in_file_), ' '.join(exc.args))
         return None
 
     df.to_parquet(out_file_)
 
 
 def proc_prism(in_file_, elev, out_file_):
-    """
-    https://daymet.ornl.gov/overview
-    """
-    df = pd.read_parquet(in_file_)
-
     try:
-        df.rename(columns={'vp': 'vpd'}, inplace=True)
-        df['vpd'] /= 1000.
+        df = pd.read_parquet(in_file_)
+
+        df['vpd'] = (df['vpdmax'] + df['vpdmin']) * 0.5 * 0.1
         es = 0.6108 * np.exp(17.27 * df['tmean'] / (df['tmean'] + 237.3))
         ea = es - df['vpd']
         df['q'] = (0.622 * ea) / (calcs._air_pressure(elev) - (0.378 * ea))
 
-        # ((srad(W / m2) * dayl(s / df['dayl'])) / 1e6)
+        df['prcp'] = df['ppt']
 
         df.index = pd.to_datetime(df.index)
         df['year'] = [i.year for i in df.index]
         df['doy'] = [i.dayofyear for i in df.index]
 
-    except KeyError as exc:
+    except (KeyError, pyarrow.lib.ArrowInvalid, FileNotFoundError) as exc:
         bad_files = os.path.join(os.path.dirname(__file__), 'bad_files.txt')
         with open(bad_files, 'a') as f:
-            f.write(in_file_ + '\n')
+            f.write(in_file_ + ' ' + ' '.join(exc.args) + '\n')
+        print(os.path.basename(in_file_), ' '.join(exc.args))
         return None
 
     df.to_parquet(out_file_)
 
 
-def proc_daymet(in_file_, lat, elev, out_file_):
-    df = pd.read_parquet(in_file_)
-
+def proc_daymet(in_file_, elev, out_file_):
     try:
+        df = pd.read_parquet(in_file_)
 
+        df['tmean'] = (df['tmin'] + df['tmax']) * 0.5
         es = 0.6108 * np.exp(17.27 * df['tmean'] / (df['tmean'] + 237.3))
-        df['vpd'] = (df['vpdmin'] + df['vpdmax']) * 0.5
+        df['vpd'] = df['vp'] / 1000.
         ea = es - df['vpd']
         df['q'] = (0.622 * ea) / (calcs._air_pressure(elev) - (0.378 * ea))
 
@@ -236,21 +274,16 @@ def proc_daymet(in_file_, lat, elev, out_file_):
         df['year'] = [i.year for i in df.index]
         df['doy'] = [i.dayofyear for i in df.index]
 
-        df['ea'] = calcs._actual_vapor_pressure(pair=calcs._air_pressure(elev),
-                                                q=df['q'])
+        df['ea'] = calcs._actual_vapor_pressure(pair=calcs._air_pressure(elev), q=df['q'])
 
-        df['rsds'] = df['srad'] * 0.0864
-        df['wind'] = df['vs']
+        # intentionally verbose
+        df['rsds'] = df['srad'] * 0.0864 * df['dayl'] / 86400
 
-        asce_params = df.parallel_apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
-        # asce_params = df.apply(calc_asce_params, lat=lat, elev=elev, zw=10, axis=1)
-        df[['tmean', 'vpd', 'rn', 'u2', 'eto']] = pd.DataFrame(asce_params.tolist(),
-                                                               index=df.index)
-
-    except KeyError as exc:
+    except (KeyError, pyarrow.lib.ArrowInvalid, FileNotFoundError) as exc:
         bad_files = os.path.join(os.path.dirname(__file__), 'bad_files.txt')
         with open(bad_files, 'a') as f:
-            f.write(in_file_ + '\n')
+            f.write(in_file_ + ' ' + ' '.join(exc.args) + '\n')
+        print(os.path.basename(in_file_), ' '.join(exc.args))
         return None
 
     df.to_parquet(out_file_)
@@ -263,16 +296,27 @@ if __name__ == '__main__':
         d = os.path.join(home, 'data', 'IrrigationGIS')
 
     overwrite = True
-    processing_targets = {'nldas2': False, 'gridmet': False,
-                          'prism': False, 'daymet': True}
+    processing_targets = {'nldas2': True, 'gridmet': True,
+                          'prism': True, 'daymet': True}
 
-    pandarallel.initialize(nb_workers=4)
+    parallel_ = True
+    if parallel_:
+        pandarallel.initialize(nb_workers=4)
 
-    sites = os.path.join(d, 'dads', 'met', 'stations', 'madis_29OCT2024.csv')
+    network = 'ghcn'
+
+    if network == 'madis':
+        sites = os.path.join(d, 'dads', 'met', 'stations', 'madis_mgrs_28OCT2024.csv')
+
+    elif network == 'ghcn':
+        sites = os.path.join(d, 'dads', 'met', 'stations', 'ghcn_CANUSA_stations_mgrs.csv')
 
     grid_dirs = os.path.join(d, 'dads', 'met', 'gridded')
 
-    extract_met_data(sites, grid_dirs, overwrite=overwrite, station_type='madis', shuffle=True,
-                     bounds=None, hourly=False, **processing_targets)
+    # daymet bounds
+    bounds = (-178., 7., -53., 83.)
+
+    extract_met_data(sites, grid_dirs, overwrite=overwrite, station_type=network, shuffle=True,
+                     bounds=bounds, hourly=False, pd_parallel=parallel_, **processing_targets)
 
 # ========================= EOF ====================================================================
