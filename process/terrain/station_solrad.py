18280f6fa9c889380e484d7016f21f1db3e16f13
dgketchum <dgketchum@gmail.com>
2025-03-17 12:40:19 -0600

    write ghcn solrad by station

--- process/terrain/station_solrad.py
+++ process/terrain/station_solrad.py
@@ -56,16 +56,16 @@ if __name__ == '__main__':
     out = os.path.join(d, 'dem', 'terrain', 'station_data')
 
     # shapefile_path_ = os.path.join(d, 'dads', 'met', 'stations', 'dads_stations_res_elev_mgrs.csv')
-    # shapefile_path_ = os.path.join(d, 'climate', 'ghcn', 'stations', 'ghcn_CANUSA_stations_mgrs.csv')
-    shapefile_path_ = os.path.join(d,'dads',  'met', 'stations', 'madis_mgrs_28OCT2024.csv')
+    shapefile_path_ = os.path.join(d, 'climate', 'ghcn', 'stations', 'ghcn_CANUSA_stations_mgrs.csv')
+    # shapefile_path_ = os.path.join(d,'dads',  'met', 'stations', 'madis_mgrs_28OCT2024.csv')
 
     # solrad_tiled = os.path.join(d, 'dads', 'dem', 'rsun_tables', 'madis')
-    # solrad_tiled = os.path.join(d, 'dads', 'dem', 'rsun_tables', 'ghcn')
-    solrad_tiled = os.path.join(d, 'dads', 'dem', 'rsun_tables', 'madis_27OCT2024')
+    solrad_tiled = os.path.join(d, 'dads', 'dem', 'rsun_tables', 'ghcn')
+    # solrad_tiled = os.path.join(d, 'dads', 'dem', 'rsun_tables', 'madis_27OCT2024')
 
     solrad_out = os.path.join(d, 'dads', 'dem', 'rsun_tables', 'station_rsun')
 
-    write_station_solrad(shapefile_path_, solrad_tiled, solrad_out, num_workers=1,
+    write_station_solrad(shapefile_path_, solrad_tiled, solrad_out, num_workers=10,
                          shuffle=True, overwrite=False, debug=False)
 
 # ========================= EOF ====================================================================
