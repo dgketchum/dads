ccf9b0aa786084a0d8cd8ce3063b45c2d8e517a8
dgketchum <dgketchum@gmail.com>
2025-06-20 12:47:35 -0600

    reprocessing interrupted rsun runs

--- process/terrain/solrad_timeseries.py
+++ process/terrain/solrad_timeseries.py
@@ -1,53 +1,125 @@
 import os
+import csv
 
 import geopandas as gpd
 import numpy as np
 import pandas as pd
+import rasterio
 from rasterstats import zonal_stats
 from concurrent.futures import ProcessPoolExecutor
+import rasterio
 
 
-def process_tile(tile, points, raster_dir, table_out, index_col='fid', overwrite=False):
-    """"""
-    file_ = os.path.join(table_out, 'tile_{}.csv'.format(tile))
-    if os.path.exists(file_) and not overwrite:
-        return
+def process_tile(tile, points, raster_dir, out_dir, index_col='fid', overwrite=False, bad_tiles=None):
+
+    if bad_tiles:
+        with open(bad_tiles, 'r') as f:
+            tile_list = [line.strip() for line in f]
+        if tile in tile_list:
+            print(f'{tile} was already found to be bad')
+            return
 
     tile_points = points[points['MGRS_TILE'] == tile]
+    station_ct = tile_points.shape[0]
+    if tile_points.empty:
+        return
+
+    complete_stations = [f.split('.')[0] for f in os.listdir(out_dir)]
+    complete_tile_stations = [s for s in tile_points.index if s in complete_stations]
+
+    if all([s in complete_tile_stations for s in tile_points.index]) and not overwrite:
+        print(f'{tile} {len(tile_points)} tile points already processed, skipping')
+        return
+    elif overwrite:
+        pass
+    else:
+        tile_points = tile_points.loc[[i for i in tile_points.index if i not in complete_tile_stations]]
+
     tile_dir = os.path.join(raster_dir, tile)
     results = {k: {} for k, v in tile_points.iterrows()}
 
-    first, missing = True, False
+    first_raster_file = None
+    for day in range(1, 366):
+        potential_raster = os.path.join(tile_dir, f'irradiance_day_{day}_{tile}.tif')
+        if os.path.exists(potential_raster):
+            first_raster_file = potential_raster
+            break
+
+    if not first_raster_file:
+        print(f'did not find a raster file for {tile}')
+        return
+
+    with rasterio.open(first_raster_file) as src:
+        raster_crs = src.crs
+
+    points_crs = tile_points.crs
+
+    if not points_crs.equals(raster_crs):
+        tile_points = tile_points.to_crs(raster_crs)
+
+    first, missing, bad_file_ct, values = True, False, False, None
+    print(f'process {tile_points.shape[0]} of {station_ct} from {tile}')
     for day in range(1, 366):
         raster_file = os.path.join(tile_dir, f'irradiance_day_{day}_{tile}.tif')
 
-        if not os.path.exists(raster_file):
-            print(f"Warning: Raster file not found for tile {tile}, day {day}. Skipping.")
-            return
+        try:
+            values = zonal_stats(tile_points, raster_file, stats="mean", geojson_out=True)
+        except rasterio.errors.RasterioIOError:
+            print(f'{raster_file}', flush=True)
+            bad_file_ct += 1
+            if bad_tiles:
+                with open(bad_tiles, 'a') as f:
+                    f.write(f'{raster_file}\n')
+                continue
 
-        values = zonal_stats(tile_points, raster_file, stats="mean", geojson_out=True)
+        for i, v in enumerate(values):
+            point_index = tile_points.index[i]
+            mean_val = v['properties']['mean']
 
-        for v in values:
-
-            if v['properties']['mean'] is None:
-                print('Empty value in {} for {}'.format(tile, v['properties'][index_col]))
-                return
+            if mean_val is None:
+                print(f'{raster_file}', flush=True)
+                bad_file_ct += 1
+                if bad_tiles:
+                    os.remove(raster_file)
+                    with open(bad_tiles, 'a') as f:
+                        f.write(f'{raster_file}\n')
+                    break
 
             if first:
-                results[v['id']] = {day: v['properties']['mean']}
-                first = False
+                results[point_index] = {day: mean_val}
             else:
-                results[v['id']][day] = v['properties']['mean']
+                results[point_index][day] = mean_val
 
-    df = pd.DataFrame(results)
-    df.to_csv(file_)
-    print(os.path.basename(file_))
+        if first:
+            first = False
+
+    if bad_file_ct > 0:
+        return
+
+    df = pd.DataFrame.from_dict(results, orient='index')
+    df.index.name = index_col
+    df = df.reindex(sorted(df.columns), axis=1)
+
+    ct = 0
+    for i, r in df.iterrows():
+        sf = os.path.join(out_dir, f'{i}.csv')
+
+        if os.path.exists(sf) and not overwrite:
+            continue
+
+        r.to_csv(sf)
+        ct += 1
+
+    print(f'tile {tile}, {ct} new stations written of {station_ct}')
 
 
 def extract_raster_values_by_tile(shapefile_path, raster_dir, table_out, shuffle=False, avoid_tiles=None,
-                                  target_tiles=None, overwrite=False, num_workers=2, index_col='fid', debug=False):
+                                  write_errors=None, target_tiles=None, overwrite=False, num_workers=2,
+                                  index_col='fid', debug=False):
+
     points = gpd.read_file(shapefile_path)
     points.index = points[index_col]
+
     if shuffle:
         points = points.sample(frac=1)
 
@@ -65,11 +137,13 @@ def extract_raster_values_by_tile(shapefile_path, raster_dir, table_out, shuffle
 
     if debug:
         for tile in tiles:
-            process_tile(tile, points.copy(), raster_dir, table_out, index_col, overwrite)
+            process_tile(tile, points.copy(), raster_dir, table_out, index_col, overwrite, write_errors)
 
-    with ProcessPoolExecutor(max_workers=num_workers) as executor:
-        executor.map(process_tile, tiles, [points] * len(tiles), [raster_dir] * len(tiles),
-                     [table_out] * len(tiles), [index_col] * len(tiles), [overwrite] * len(tiles))
+    else:
+        with ProcessPoolExecutor(max_workers=num_workers) as executor:
+            executor.map(process_tile, tiles, [points] * len(tiles), [raster_dir] * len(tiles),
+                         [table_out] * len(tiles), [index_col] * len(tiles), [overwrite] * len(tiles),
+                         [write_errors] * len(tiles))
 
 
 if __name__ == '__main__':
@@ -77,16 +151,43 @@ if __name__ == '__main__':
     if not os.path.exists(root):
         root = '/home/dgketchum/data/IrrigationGIS'
 
-    # this must be EPSG:5071 shapefile
-    # shapefile_path_ = os.path.join(root, 'dads', 'met', 'stations', 'madis_mgrs_28OCT2024_3978.shp')
+    raster_dir_ = os.path.join(root, 'dads', 'dem', 'rsun_irradiance')
+    solrad_out = os.path.join(root, 'dads', 'dem', 'rsun_stations')
 
-    shapefile_path_ = os.path.join(root, 'dads', 'met', 'stations', 'madis_17MAY2025_gap_mgrs_5071.shp')
+    # bad_tiles_file = os.path.join(root, 'dads', 'dem', 'bad_tiles.txt')
+    # if not os.path.isfile(bad_tiles_file):
+    #     with open(bad_tiles_file, 'w'):
+    #         pass
 
-    raster_dir_ = os.path.join(root, 'dads', 'dem', 'rsun')
-    solrad_out = os.path.join(root, 'dads', 'dem', 'rsun_tables', 'madis_17MAY2025_gap')
-    # solrad_out = os.path.join(root, 'dads', 'dem', 'rsun_tables', 'ghcn')
+    bad_tiles_file = os.path.join(root, 'dads', 'dem', 'bad_tiles.txt')
+    with open(bad_tiles_file, 'r') as f:
+        lines = f.readlines()
+
+    bad_files, total_files = {}, 0
+    for line in lines:
+        splt = line.split(os.path.sep)
+        try:
+            tile = splt[8]
+        except IndexError:
+            continue
+
+        doy = int(splt[9].split('_')[2])
+        if tile not in bad_files:
+            bad_files[tile] = [doy]
+        else:
+            bad_files[tile].append(doy)
+        total_files += 1
+
+    tiles = list(bad_files.keys())
+
+    shapefile_path_ = os.path.join(root, 'dads', 'met', 'stations', 'madis_17MAY2025_mgrs.shp')
     extract_raster_values_by_tile(shapefile_path_, raster_dir_, solrad_out, num_workers=16, avoid_tiles=None,
                                   target_tiles=None, shuffle=True, overwrite=False, index_col='fid',
-                                  debug=False)
+                                  debug=False, write_errors=None)
+
+    # shapefile_path_ = os.path.join(root, 'climate', 'ghcn', 'stations', 'ghcn_stations_mgrs_country.shp')
+    # extract_raster_values_by_tile(shapefile_path_, raster_dir_, solrad_out, num_workers=16, avoid_tiles=None,
+    #                               target_tiles=None, shuffle=True, overwrite=False, index_col='STAID',
+    #                               debug=True)
 
 # ========================= EOF ====================================================================
--- process/terrain/sun.py
+++ process/terrain/sun.py
@@ -15,7 +15,7 @@ def ingest_rasters(in_dir, tiles, mapset, overwrite=False):
 
         tile = in_dem.split('.')[0][-5:]
 
-        if tile not in tiles:
+        if tiles and tile not in tiles:
             continue
 
         dem_name = f'dem_{tile}'
@@ -29,7 +29,7 @@ def ingest_rasters(in_dir, tiles, mapset, overwrite=False):
 
 def worker_calculate_single_tile_irradiance(args):
     """"""
-    dem_name_grass, dem_file_path, terrain_dir, mapset, r_sun_nprocs, overwrite = args
+    dem_name_grass, dem_file_path, terrain_dir, mapset, r_sun_nprocs, overwrite, doys = args
 
     tile = dem_name_grass.split('_')[-1]
 
@@ -45,10 +45,6 @@ def worker_calculate_single_tile_irradiance(args):
     slope_cmd = ['gdaldem', 'slope', dem_file_path, slope_output_tif]
     aspect_cmd = ['gdaldem', 'aspect', dem_file_path, aspect_output_tif]
 
-    if overwrite:
-        slope_cmd += ['--overwrite']
-        aspect_cmd += ['--overwrite']
-
     subprocess.run(slope_cmd, check=True)
     subprocess.run(aspect_cmd, check=True)
 
@@ -61,18 +57,24 @@ def worker_calculate_single_tile_irradiance(args):
     subprocess.run(['g.region', f'rast={dem_name_grass}@{mapset}'], check=True)
 
     for day in range(1, 366):
+
+        if doys and day not in doys:
+            continue
+
         irradiance_grass_name = f'irradiance_day_{day}_{tile}'
 
-        rsun_command_list = ['r.sun',
+        rsun_command_list = [f'r.sun',
                              f'elevation={dem_name_grass}@{mapset}',
                              f'slope={slope_grass_name}@{mapset}',
                              f'aspect={aspect_grass_name}@{mapset}',
                              f'day={day}',
                              f'glob_rad={irradiance_grass_name}',
-                             f'nprocs={r_sun_nprocs}', '--overwrite']
+                             f'nprocs={r_sun_nprocs}',
+                             f'--overwrite']
 
         try:
             subprocess.run(rsun_command_list, check=True)
+            print(f'......................................{irradiance_grass_name}')
         except subprocess.CalledProcessError:
             print(f'{irradiance_grass_name} failed')
             continue
@@ -82,12 +84,13 @@ def worker_calculate_single_tile_irradiance(args):
 
 def calculate_terrain_irradiance_parallel(terrain_dir, terrain_source_path,
                                           mapset='PERMANENT', tiles_filter=None,
-                                          overwrite=False,
+                                          overwrite=False, specific_bad_files=None,
                                           num_parallel_tiles=None, r_sun_nprocs_per_tile=1):
     """"""
     source_dem_tif_filenames = sorted([f for f in os.listdir(terrain_source_path) if f.endswith('.tif')])
 
     task_args_list = []
+    doys = None
     for dem_tif_filename in source_dem_tif_filenames:
         dem_name_grass = dem_tif_filename.split('.')[0]
         tile_id = dem_name_grass.split('_')[-1]
@@ -95,27 +98,33 @@ def calculate_terrain_irradiance_parallel(terrain_dir, terrain_source_path,
         if tiles_filter and tile_id not in tiles_filter:
             continue
 
-        if not overwrite:
+        if specific_bad_files:
+            doys = specific_bad_files[tile_id]
+
+        elif not overwrite:
             try:
                 glist_result = subprocess.run(
                     ['g.list', 'type=raster', f'pattern=irradiance_day_*_{tile_id}'],
                     capture_output=True, text=True, check=True
                 )
                 existing_irradiance_files = [r for r in glist_result.stdout.strip().split('\n') if r]
+
                 if len(existing_irradiance_files) >= 365:
                     print(f'{tile_id} is complete, skipping')
                     continue
+
                 else:
                     print(f'{len(existing_irradiance_files)} files complete: adding {tile_id}')
+
             except subprocess.CalledProcessError as e:
                 print(f'error {e} on {tile_id}')
 
         dem_file_full_path = os.path.join(terrain_source_path, dem_tif_filename)
         task_args_list.append((dem_name_grass, dem_file_full_path, terrain_dir,
-                               mapset, r_sun_nprocs_per_tile, overwrite))
+                               mapset, r_sun_nprocs_per_tile, overwrite, doys))
 
     if num_parallel_tiles is None:
-        num_parallel_tiles = cpu_count()
+        num_parallel_tiles = 1
 
     collected_results = []
     if task_args_list and num_parallel_tiles > 0:
@@ -137,7 +146,6 @@ def export_rasters(terrain_dir, rsun_out, mapset='PERMANENT', overwrite=False, m
 
         tile = dem_name.split('_')[-1]
         if mgrs_list and tile not in mgrs_list:
-            print(f'{tile} exists but is not in list')
             continue
 
         # print('\n', tile)
@@ -146,19 +154,17 @@ def export_rasters(terrain_dir, rsun_out, mapset='PERMANENT', overwrite=False, m
         if not os.path.isdir(tile_dir):
             os.mkdir(tile_dir)
 
+        tile_contents = [f for f in os.listdir(tile_dir) if '.tif' in f]
+        if len(tile_contents) >= 365 and not overwrite:
+            print(f'tile {tile} already exists, skipping')
+            continue
+
         subprocess.call(['g.region', f'rast=dem_{tile}@{mapset}'])
 
         first = True
         for day in range(1, 366):
-
             irradiance_output_tif = os.path.join(tile_dir, 'irradiance_day_{0}_{1}.tif'.format(day, tile))
             irradiance_input = 'irradiance_day_{0}_{1}'.format(day, tile)
-
-            if os.path.exists(irradiance_output_tif) and not overwrite:
-                if first:
-                    print(irradiance_output_tif, 'exists')
-                    first = False
-                continue
 
             subprocess.call(
                 ['r.out.gdal', '-c',
@@ -178,7 +184,7 @@ if __name__ == '__main__':
 
     _bucket = 'gs://wudr'
     station_set = 'ghcn'
-    zone = 'canada'
+    zone = 'alaska'
 
     if station_set == 'madis':
         stations = 'madis_17MAY2025_gap_mgrs'
@@ -224,17 +230,40 @@ if __name__ == '__main__':
     if zone == 'canada':
         sites_df = sites_df[sites_df['AFF_ISO'] == 'CA']
 
-    tiles = sites_df['MGRS_TILE'].unique().tolist()
-    tiles = [m for m in tiles if isinstance(m, str)]
-    mgrs_tiles = list(set(tiles))
-    mgrs_tiles.sort()
+    zone_tiles = [d[4:9] for d in os.listdir(tif_dem)]
 
-    # ingest_rasters(tif_dem, mgrs_tiles, mapset=mapset_, overwrite=True)
+    bad_tiles_file = os.path.join(root, 'dads', 'dem', 'bad_tiles.txt')
+    with open(bad_tiles_file, 'r') as f:
+        lines = f.readlines()
 
-    # calculate_terrain_irradiance_parallel(dem_d, tif_dem, mapset=mapset_, tiles_filter=mgrs_tiles, overwrite=False,
-    #                                       num_parallel_tiles=1, r_sun_nprocs_per_tile=4)
+    bad_files, total_files = {}, 0
+    for line in lines:
+        splt = line.split(os.path.sep)
+        try:
+            tile = splt[8]
+        except IndexError:
+            continue
 
-    rsun_out_ = os.path.join(dem_d, 'rsun_irradiance', f'rsun_{epsg}')
+        if tile not in zone_tiles:
+            continue
+
+        doy = int(splt[9].split('_')[2])
+        if tile not in bad_files:
+            bad_files[tile] = [doy]
+        else:
+            bad_files[tile].append(doy)
+        total_files += 1
+
+    tiles = list(bad_files.keys())
+
+    ingest_rasters(tif_dem, tiles, mapset=mapset_, overwrite=True)
+
+    rsun_out_ = os.path.join(root, 'dads', 'dem', 'rsun_irradiance')
+
+    calculate_terrain_irradiance_parallel(dem_d, tif_dem, mapset=mapset_, tiles_filter=tiles,
+                                          overwrite=True, specific_bad_files=bad_files,
+                                          num_parallel_tiles=4, r_sun_nprocs_per_tile=4)
+
     export_rasters(tif_dem, rsun_out_, mapset=mapset_, mgrs_list=tiles, overwrite=True)
 
 # ========================= EOF ====================================================================
