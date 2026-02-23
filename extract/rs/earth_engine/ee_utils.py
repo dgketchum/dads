22a2fd15ab91dc9110c593cd258d0926b06d1a43
dgketchum <dgketchum@gmail.com>
2026-02-23 08:57:03 -0700

    seamless PNW 1km DEM export via Earth Engine

--- extract/rs/earth_engine/ee_utils.py
+++ extract/rs/earth_engine/ee_utils.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 
 import ee
@@ -6,85 +7,174 @@ import ee
 def landsat_c2_sr(input_img):
     # credit: cgmorton; https://github.com/Open-ET/openet-core-beta/blob/master/openet/core/common.py
 
-    INPUT_BANDS = ee.Dictionary({
-        'LANDSAT_4': ['SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B7',
-                      'ST_B6', 'QA_PIXEL', 'QA_RADSAT'],
-        'LANDSAT_5': ['SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B7',
-                      'ST_B6', 'QA_PIXEL', 'QA_RADSAT'],
-        'LANDSAT_7': ['SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B7',
-                      'ST_B6', 'QA_PIXEL', 'QA_RADSAT'],
-        'LANDSAT_8': ['SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7',
-                      'ST_B10', 'QA_PIXEL', 'QA_RADSAT'],
-        'LANDSAT_9': ['SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7',
-                      'ST_B10', 'QA_PIXEL', 'QA_RADSAT'],
-    })
-    OUTPUT_BANDS = ['B2', 'B3', 'B4', 'B5', 'B6', 'B7',
-                    'B10', 'QA_PIXEL', 'QA_RADSAT']
+    INPUT_BANDS = ee.Dictionary(
+        {
+            "LANDSAT_4": [
+                "SR_B1",
+                "SR_B2",
+                "SR_B3",
+                "SR_B4",
+                "SR_B5",
+                "SR_B7",
+                "ST_B6",
+                "QA_PIXEL",
+                "QA_RADSAT",
+            ],
+            "LANDSAT_5": [
+                "SR_B1",
+                "SR_B2",
+                "SR_B3",
+                "SR_B4",
+                "SR_B5",
+                "SR_B7",
+                "ST_B6",
+                "QA_PIXEL",
+                "QA_RADSAT",
+            ],
+            "LANDSAT_7": [
+                "SR_B1",
+                "SR_B2",
+                "SR_B3",
+                "SR_B4",
+                "SR_B5",
+                "SR_B7",
+                "ST_B6",
+                "QA_PIXEL",
+                "QA_RADSAT",
+            ],
+            "LANDSAT_8": [
+                "SR_B2",
+                "SR_B3",
+                "SR_B4",
+                "SR_B5",
+                "SR_B6",
+                "SR_B7",
+                "ST_B10",
+                "QA_PIXEL",
+                "QA_RADSAT",
+            ],
+            "LANDSAT_9": [
+                "SR_B2",
+                "SR_B3",
+                "SR_B4",
+                "SR_B5",
+                "SR_B6",
+                "SR_B7",
+                "ST_B10",
+                "QA_PIXEL",
+                "QA_RADSAT",
+            ],
+        }
+    )
+    OUTPUT_BANDS = ["B2", "B3", "B4", "B5", "B6", "B7", "B10", "QA_PIXEL", "QA_RADSAT"]
 
-    spacecraft_id = ee.String(input_img.get('SPACECRAFT_ID'))
+    spacecraft_id = ee.String(input_img.get("SPACECRAFT_ID"))
 
-    prep_image = input_img \
-        .select(INPUT_BANDS.get(spacecraft_id), OUTPUT_BANDS) \
-        .multiply([0.0000275, 0.0000275, 0.0000275, 0.0000275,
-                   0.0000275, 0.0000275, 0.00341802, 1, 1]) \
+    prep_image = (
+        input_img.select(INPUT_BANDS.get(spacecraft_id), OUTPUT_BANDS)
+        .multiply(
+            [
+                0.0000275,
+                0.0000275,
+                0.0000275,
+                0.0000275,
+                0.0000275,
+                0.0000275,
+                0.00341802,
+                1,
+                1,
+            ]
+        )
         .add([-0.2, -0.2, -0.2, -0.2, -0.2, -0.2, 149.0, 0, 0])
+    )
 
     def _cloud_mask(i):
-        qa_img = i.select(['QA_PIXEL'])
+        qa_img = i.select(["QA_PIXEL"])
         cloud_mask = qa_img.rightShift(3).bitwiseAnd(1).neq(0)
         cloud_mask = cloud_mask.Or(qa_img.rightShift(2).bitwiseAnd(1).neq(0))
         cloud_mask = cloud_mask.Or(qa_img.rightShift(1).bitwiseAnd(1).neq(0))
         cloud_mask = cloud_mask.Or(qa_img.rightShift(4).bitwiseAnd(1).neq(0))
         cloud_mask = cloud_mask.Or(qa_img.rightShift(5).bitwiseAnd(1).neq(0))
-        sat_mask = i.select(['QA_RADSAT']).gt(0)
+        sat_mask = i.select(["QA_RADSAT"]).gt(0)
         cloud_mask = cloud_mask.Or(sat_mask)
 
-        cloud_mask = cloud_mask.Not().rename(['cloud_mask'])
+        cloud_mask = cloud_mask.Not().rename(["cloud_mask"])
 
         return cloud_mask
 
     mask = _cloud_mask(input_img)
 
-    image = prep_image.updateMask(mask).copyProperties(input_img, ['system:time_start'])
+    image = prep_image.updateMask(mask).copyProperties(input_img, ["system:time_start"])
 
     return image
 
 
 def landsat_masked(yr):
-    start = '{}-01-01'.format(yr)
-    end_date = '{}-01-01'.format(yr + 1)
+    start = "{}-01-01".format(yr)
+    end_date = "{}-01-01".format(yr + 1)
 
-    l4_coll = ee.ImageCollection('LANDSAT/LT04/C02/T1_L2').filterDate(start, end_date).map(landsat_c2_sr)
-    l5_coll = ee.ImageCollection('LANDSAT/LT05/C02/T1_L2').filterDate(start, end_date).map(landsat_c2_sr)
-    l7_coll = ee.ImageCollection('LANDSAT/LE07/C02/T1_L2').filterDate(start, end_date).map(landsat_c2_sr)
-    l8_coll = ee.ImageCollection('LANDSAT/LC08/C02/T1_L2').filterDate(start, end_date).map(landsat_c2_sr)
-    l9_coll = ee.ImageCollection('LANDSAT/LC09/C02/T1_L2').filterDate(start, end_date).map(landsat_c2_sr)
+    l4_coll = (
+        ee.ImageCollection("LANDSAT/LT04/C02/T1_L2")
+        .filterDate(start, end_date)
+        .map(landsat_c2_sr)
+    )
+    l5_coll = (
+        ee.ImageCollection("LANDSAT/LT05/C02/T1_L2")
+        .filterDate(start, end_date)
+        .map(landsat_c2_sr)
+    )
+    l7_coll = (
+        ee.ImageCollection("LANDSAT/LE07/C02/T1_L2")
+        .filterDate(start, end_date)
+        .map(landsat_c2_sr)
+    )
+    l8_coll = (
+        ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
+        .filterDate(start, end_date)
+        .map(landsat_c2_sr)
+    )
+    l9_coll = (
+        ee.ImageCollection("LANDSAT/LC09/C02/T1_L2")
+        .filterDate(start, end_date)
+        .map(landsat_c2_sr)
+    )
 
-    lsSR_masked = ee.ImageCollection(l7_coll.merge(l8_coll).merge(l9_coll).merge(l5_coll).merge(l4_coll))
+    lsSR_masked = ee.ImageCollection(
+        l7_coll.merge(l8_coll).merge(l9_coll).merge(l5_coll).merge(l4_coll)
+    )
 
     return lsSR_masked
 
 
 def landsat_composites(year, start, end, append_name, scale=False):
-    start_year = datetime.strptime(start, '%Y-%m-%d').year
+    start_year = datetime.strptime(start, "%Y-%m-%d").year
     if start_year != year:
         year = start_year
 
     lsSR_masked = landsat_masked(year)
-    thermal = ee.Image(lsSR_masked.filterDate(start, end).map(
-        lambda x: x.select(['B10'],
-                           ['B10_{}'.format(append_name)]
-                           )).mean())
+    thermal = ee.Image(
+        lsSR_masked.filterDate(start, end)
+        .map(lambda x: x.select(["B10"], ["B10_{}".format(append_name)]))
+        .mean()
+    )
 
-    bands_means = ee.Image(lsSR_masked.filterDate(start, end).map(
-        lambda x: x.select(['B2', 'B3', 'B4', 'B5', 'B6', 'B7'],
-                           ['B2_{}'.format(append_name),
-                            'B3_{}'.format(append_name),
-                            'B4_{}'.format(append_name),
-                            'B5_{}'.format(append_name),
-                            'B6_{}'.format(append_name),
-                            'B7_{}'.format(append_name)]
-                           )).mean())
+    bands_means = ee.Image(
+        lsSR_masked.filterDate(start, end)
+        .map(
+            lambda x: x.select(
+                ["B2", "B3", "B4", "B5", "B6", "B7"],
+                [
+                    "B2_{}".format(append_name),
+                    "B3_{}".format(append_name),
+                    "B4_{}".format(append_name),
+                    "B5_{}".format(append_name),
+                    "B6_{}".format(append_name),
+                    "B7_{}".format(append_name),
+                ],
+            )
+        )
+        .mean()
+    )
     if scale:
         thermal = thermal.multiply(100).int()
         bands_means = bands_means.multiply(10000).int()
@@ -93,16 +183,23 @@ def landsat_composites(year, start, end, append_name, scale=False):
     return bands_means
 
 
-def is_authorized():
+def is_authorized(project=None):
+    if project is None:
+        project = os.environ.get("EEPROJECT")
+    if not project:
+        raise RuntimeError(
+            "Earth Engine project not set. Export EEPROJECT=<your-project-id> "
+            "(e.g. EEPROJECT=ee-dgketchum)"
+        )
     try:
-        ee.Initialize(project='ee-dgketchum')
-        print('Authorized')
+        ee.Initialize(project=project)
+        print(f"Authorized ({project})")
     except Exception as e:
-        print('You are not authorized: {}'.format(e))
+        print("You are not authorized: {}".format(e))
         exit(1)
     return None
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
 # ========================= EOF ====================================================================
--- extract/terrain/ee_dem.py
+++ extract/terrain/ee_dem.py
@@ -5,104 +5,150 @@ import time
 import ee
 import pandas as pd
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath("../.."))
 from extract.rs.earth_engine.ee_utils import is_authorized
 from utils.station_parameters import station_par_map
 
 sys.setrecursionlimit(2000)
 
-BOUNDARIES = 'users/dgketchum/boundaries'
+BOUNDARIES = "users/dgketchum/boundaries"
 
 
-def export_dem(tiles, check_dir=None, crs_epsg='5071', dry_run=False):
+def export_dem(tiles, check_dir=None, crs_epsg="5071", dry_run=False):
     """"""
 
-    mgrs = ee.FeatureCollection('users/dgketchum/boundaries/MGRS_TILE')
+    mgrs = ee.FeatureCollection("users/dgketchum/boundaries/MGRS_TILE")
 
     for tile in tiles:
-
-        desc = 'dem_{}'.format(tile)
+        desc = "dem_{}".format(tile)
 
         if check_dir:
-            outfile = os.path.join(check_dir, '{}.tif'.format(desc))
+            outfile = os.path.join(check_dir, "{}.tif".format(desc))
             if os.path.exists(outfile):
-                print('{} exists'.format(outfile))
+                print("{} exists".format(outfile))
                 continue
 
             else:
                 if dry_run:
-                    print(f'export {outfile}')
+                    print(f"export {outfile}")
                     continue
 
-        clip = mgrs.filterMetadata('MGRS_TILE', 'equals', tile)
-        coords = clip.getInfo()['features'][0]['geometry']['coordinates'][0]
+        clip = mgrs.filterMetadata("MGRS_TILE", "equals", tile)
+        coords = clip.getInfo()["features"][0]["geometry"]["coordinates"][0]
         max_lat = max([abs(c[1]) for c in coords])
 
         if max_lat < 59.0:
             # Good to 59 degrees latitude
-            ned = ee.Image('USGS/SRTMGL1_003').select(['elevation'])
-            elev = ee.Terrain.products(ned).select(['elevation'])
+            ned = ee.Image("USGS/SRTMGL1_003").select(["elevation"])
+            elev = ee.Terrain.products(ned).select(["elevation"])
         else:
             # Good in arctic regions
-            dataset = ee.ImageCollection('JAXA/ALOS/AW3D30/V3_2')
-            elev = dataset.select('DSM').mosaic()
+            dataset = ee.ImageCollection("JAXA/ALOS/AW3D30/V3_2")
+            elev = dataset.select("DSM").mosaic()
 
         img = elev.clip(clip.first().geometry().buffer(1000))
-        bucket_file = os.path.join(f'dem_{crs_epsg}', desc)
+        bucket_file = os.path.join(f"dem_{crs_epsg}", desc)
 
         task = ee.batch.Export.image.toCloudStorage(
             image=img,
             description=desc,
-            bucket='wudr',
+            bucket="wudr",
             fileNamePrefix=bucket_file,
             scale=250,
-            crs=f'EPSG:{crs_epsg}',
-            maxPixels=1e13)
+            crs=f"EPSG:{crs_epsg}",
+            maxPixels=1e13,
+        )
 
         try:
             task.start()
             print(desc)
         except ee.ee_exception.EEException as e:
-            print('{}, waiting on '.format(e), desc, '......')
+            print("{}, waiting on ".format(e), desc, "......")
             time.sleep(600)
             task.start()
             print(desc)
 
 
-if __name__ == '__main__':
+def export_pnw_1km_dem(bucket="wudr", description="dem_pnw_1km"):
+    """Export a seamless SRTM DEM aggregated to the PNW 1 km grid.
+
+    The output is pixel-aligned to prep.pnw_1km_grid (EPSG:5070, 1000 m,
+    origin -2349000, 3214000, 1758 x 1076 pixels).  SRTM 30 m is aggregated
+    via ee.Reducer.mean() to 1 km, so the result is a single continuous
+    raster with no MGRS tile seams.
+    """
+    # PNW 1 km grid constants (from prep/pnw_1km_grid.py)
+    crs = "EPSG:5070"
+    scale = 1000
+    x_origin = -2349000.0
+    y_origin = 3214000.0
+    width = 1758
+    height = 1076
+
+    crs_transform = [scale, 0, x_origin, 0, -scale, y_origin]
+
+    # Bounding geometry in EPSG:5070
+    x_min, y_max = x_origin, y_origin
+    x_max = x_min + width * scale
+    y_min = y_max - height * scale
+    region = ee.Geometry.Rectangle([x_min, y_min, x_max, y_max], crs, geodesic=False)
+
+    srtm = ee.Image("USGS/SRTMGL1_003").select(["elevation"])
+
+    task = ee.batch.Export.image.toCloudStorage(
+        image=srtm,
+        description=description,
+        bucket=bucket,
+        fileNamePrefix=f"dem_5070/{description}",
+        crs=crs,
+        crsTransform=crs_transform,
+        region=region,
+        maxPixels=1e13,
+    )
+    task.start()
+    print(f"Started EE task: {description}")
+    print(f"  Grid: {width} x {height} @ {scale} m, EPSG:5070")
+    print(f"  Origin: ({x_origin}, {y_origin})")
+    print("  Monitor: https://code.earthengine.google.com/tasks")
+    return task
+
+
+if __name__ == "__main__":
     is_authorized()
 
-    dem_d = '/data/ssd2/dads/dem'
-    d = '/nas'
+    dem_d = "/data/ssd2/dads/dem"
+    d = "/nas"
 
-    _bucket = 'gs://wudr'
-    station_set = 'ghcn'
-    zone = 'north'
+    _bucket = "gs://wudr"
+    station_set = "ghcn"
+    zone = "north"
 
-    if station_set == 'madis':
-        stations = 'madis_17MAY2025_gap_mgrs'
-        sites = os.path.join(d, 'dads', 'met', 'stations', f'{stations}.csv')
+    if station_set == "madis":
+        stations = "madis_17MAY2025_gap_mgrs"
+        sites = os.path.join(d, "dads", "met", "stations", f"{stations}.csv")
 
-    elif station_set == 'ghcn':
-        stations = 'ghcn_CANUSA_stations_mgrs'
-        sites = os.path.join(d, 'climate', 'ghcn', 'stations', 'ghcn_stations_mgrs_country.csv')
+    elif station_set == "ghcn":
+        stations = "ghcn_CANUSA_stations_mgrs"
+        sites = os.path.join(
+            d, "climate", "ghcn", "stations", "ghcn_stations_mgrs_country.csv"
+        )
 
-    elif station_set == 'ndbc':
-        stations = 'ndbc_stations'
-        sites = os.path.join(d, 'climate', 'ndbc', 'ndbc_meta', 'ndbc_stations.csv')
-    
+    elif station_set == "ndbc":
+        stations = "ndbc_stations"
+        sites = os.path.join(d, "climate", "ndbc", "ndbc_meta", "ndbc_stations.csv")
+
     else:
         raise ValueError
 
-    if zone == 'north':
-        bounds = (-180., 49., -60., 85.)
-        epsg = '3978'
-        chk = os.path.join(dem_d, f'dem_{epsg}')
+    if zone == "north":
+        bounds = (-180.0, 49.0, -60.0, 85.0)
+        epsg = "3978"
+        chk = os.path.join(dem_d, f"dem_{epsg}")
 
-    elif zone == 'south':
-        bounds = (-180., 23., -60., 49.)
-        epsg = '5071'
-        chk = os.path.join(dem_d, f'dem_{epsg}')
+    elif zone == "south":
+        bounds = (-180.0, 23.0, -60.0, 49.0)
+        epsg = "5071"
+        chk = os.path.join(dem_d, f"dem_{epsg}")
 
     else:
         raise ValueError
@@ -111,13 +157,17 @@ if __name__ == '__main__':
 
     kw = station_par_map(station_set)
 
-    sites_df = sites_df[(sites_df[kw['lat']] < bounds[3]) & (sites_df[kw['lat']] >= bounds[1])]
-    sites_df = sites_df[(sites_df[kw['lon']] < bounds[2]) & (sites_df[kw['lon']] >= bounds[0])]
+    sites_df = sites_df[
+        (sites_df[kw["lat"]] < bounds[3]) & (sites_df[kw["lat"]] >= bounds[1])
+    ]
+    sites_df = sites_df[
+        (sites_df[kw["lon"]] < bounds[2]) & (sites_df[kw["lon"]] >= bounds[0])
+    ]
 
-    if zone == 'north':
-        sites_df = sites_df[sites_df['AFF_ISO'] == 'CA']
+    if zone == "north":
+        sites_df = sites_df[sites_df["AFF_ISO"] == "CA"]
 
-    tiles = sites_df['MGRS_TILE'].unique().tolist()
+    tiles = sites_df["MGRS_TILE"].unique().tolist()
     tiles = [m for m in tiles if isinstance(m, str)]
     mgrs_tiles = list(set(tiles))
     mgrs_tiles.sort()
