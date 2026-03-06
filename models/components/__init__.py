f04892734b0efdfead204f5266c3e708cd2d08ca
dgketchum <dgketchum@gmail.com>
2026-03-06 11:44:38 -0700

    add re-exports to models/components/__init__

--- models/components/__init__.py
+++ models/components/__init__.py
@@ -1,1 +1,16 @@
 """Shared model components (GNN layers, TCN, scalers, task adapters)."""
+
+from models.components.edge_gated_gnn import (
+    EdgeGatedAttention as EdgeGatedAttention,
+    EdgeGatedGNN as EdgeGatedGNN,
+)
+from models.components.scalers import (
+    MinMaxScaler as MinMaxScaler,
+    Scaler as Scaler,
+    StandardScaler as StandardScaler,
+)
+from models.components.tasks import (
+    TaskAdapter as TaskAdapter,
+    get_task_adapter as get_task_adapter,
+)
+from models.components.tcn import TemporalConvEncoder as TemporalConvEncoder
